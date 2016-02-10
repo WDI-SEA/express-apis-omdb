745242401274e52d5bb703e0b7d171a0046a77d0
steampunklink <nethhorn@gmail.com>
2016-02-09 21:49:16 -0800

    failed to add css

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/static'));
 
 var request = require("request");
 
