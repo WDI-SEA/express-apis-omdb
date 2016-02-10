dfcc069531fb5d4c3c737fd6de7958eec1c9599c
evwilkin <evan.wilkinson@gmail.com>
2016-02-10 00:37:36 -0800

    adding styling to first two pages

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+//variables and requires
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require("request");
 
+//initiate app
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/static'));
 
+//home page
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
+//call to API & present results
 app.get('/results', function(req, res) {
 	var search = req.query.search;
 	request("http://www.omdbapi.com/?s="+search, function(error, response, body) {   
@@ -22,6 +27,7 @@ app.get('/results', function(req, res) {
 	});
 });
 
+//controllers for linking to specific movie details
 app.use('/movie', require('./controllers'))
 
 
