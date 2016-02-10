b7ace63626c7dde59a35d74005cb55e9e60059a1
Scott <scott.heron@outlook.com>
2016-02-09 19:59:12 -0800

    homework complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var ejsLayouts = require("express-ejs-layouts");
 var request = require("request");
-var searchValue;
+var searchResult;
 
 app.set("view engine", "ejs");
 app.use(ejsLayouts);
@@ -15,19 +15,34 @@ app.get("/", function(req, res){
 
 
 app.get("/show", function(req, res){
-	var searchResult = req.query.search;
+	searchResult = req.query.search;
 	request(
 		"http://omdbapi.com/?s="+searchResult, 
 		function(error, response, body){
 			if (!error && response.statusCode == 200) {
-				console.log(body);
-				res.render("show.ejs", {
+				res.render("show", {
 					movieResult: JSON.parse(body)
 				});
 			} 
 		}
 	);
-})
+});
+
+app.get("/show/movie/:imdbId", function(req, res){
+	var id = req.params.imdbId;
+	request(
+		"http://omdbapi.com/?i="+id, 
+		function(error, response, body){
+
+			if (!error && response.statusCode == 200) {
+				res.render("movie", {
+					movieResult: JSON.parse(body),
+					searchValue: searchResult
+				});
+			} 
+		}
+	);
+});
 	
 	
 
