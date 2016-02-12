84814b82aa289de5a45811196c2d2bdc0de1cb9f
aheitzen <alexaheitzenrader@yahoo.com>
2016-02-11 20:20:15 -0800

    homework

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
+var db = require('./models');
 var request = require('request');
 var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(__dirname + "/static")); //to link to the static files, dirname(node sets it) var that gives absolute path to dir we are in
 
 var ejsLayouts = require("express-ejs-layouts");
 app.use(ejsLayouts);
@@ -29,14 +32,46 @@ app.get("/movie/:imbdId", function(req, res) {
 			res.render("movie.ejs", {movie: body});
 
 		}
-
-
 	});
-
-
-	
-
 });
+
+app.post('/favorites', function(req, res) {
+	// var favorite = req.body;
+	// if(favorite.title) {
+	// 	db.favorite.create({
+	// 		imbd:favorite.imbdid,
+	// 		year:favorite.year,
+	// 		title:favorite.title
+	db.favorite.create({
+		imbdId: req.body.imdbid, //takes form input and stores it in rec.body
+		name: req.body.title,
+		year: req.body.year
+	}).then(function(saved) {
+		res.redirect('/favorites');
+	});
+});
+
+
+
+app.get('/favorites', function(req, res) {
+	db.favorite.findAll().then(function(favs){
+		res.render('favorites.ejs', {favArray: favs})
+	});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
