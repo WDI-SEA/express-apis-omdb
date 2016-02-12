2763db1ab0560e239c31ae2f440a100cca8392f8
k-buffum <knbuffum@gmail.com>
2016-02-11 21:45:10 -0800

    favorites page and button added

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 var express = require("express");
 var ejsLayouts = require("express-ejs-layouts");
 var request = require("request");
+var db = require("./models");
+var bodyParser = require("body-parser");
+
 var app = express();
 
 app.set("view engine", "ejs");
 
 app.use(ejsLayouts);
 app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use("/favorites", require("./controllers/favorites"));
+
 
 app.get("/", function(req, res) {
 	res.render("index.ejs");
 });
 
+
+//Returns results page with movie listings based on search
 app.get("/results", function(req, res) {
 	var qs = req.query.movieChoice;
-	console.log(qs);
+	//console.log(qs);
 	request( "http://omdbapi.com/?s=" + qs,
 		function (error, response, body) {
 			if(!error && response.statusCode == 200 ) {
-				console.log(body)
 				movies = JSON.parse(body);
 				res.render("results.ejs", { movies: movies });
 			}
@@ -26,9 +33,10 @@ app.get("/results", function(req, res) {
 	);
 });
 
+
+// Gets imdbID from each result based on search
 app.get("/results/:imdbID", function(req, res) {
 	var movieId = req.params.imdbID;
-	
 	request("http://omdbapi.com/?i=" + movieId,
 		function (error, response, body) {
 			if(!error && response.statusCode == 200) {
