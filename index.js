49892e81e68fadd088c8608bb1be6e27e54ae032
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-02-12 08:31:40 -0800

    sequelize finished

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+var db = require("./models");
 
 app.set("view engine" ,"ejs");
 
@@ -14,19 +17,12 @@ app.get("/", function(req, res){
 	res.render("index.ejs");
 });
 
-
-app.get("/movies/:idx", function(req, res){
-
-	var moviesIdx = req.params.idx;
-	request("http://omdbapi.com/?i="+moviesIdx,function(error, response, data){
-		if(!error && response.statusCode == 200){
-			var movieInfo = JSON.parse(data);
-			res.render("moviesShow.ejs",{movies: movieInfo});
-		}else{
-
-		};
-	});	
+app.get("/favorites",function(req,res){
+	db.favorite.findAll().then(function(favorite){
+		res.render("favorites.ejs", {favorite:favorite});
+	});
 });
+
 
 app.get("/movies", function(req, res){
 	var title = req.query.title
@@ -54,6 +50,42 @@ app.get("/movies", function(req, res){
 		});
 	
 });
+
+app.get("/movies/:idx", function(req, res){
+
+	var moviesIdx = req.params.idx;
+	request("http://omdbapi.com/?i="+moviesIdx,function(error, response, data){
+		if(!error && response.statusCode == 200){
+			var movieInfo = JSON.parse(data);
+			res.render("moviesShow.ejs",{movies: movieInfo});
+		}else{
+
+		};
+	});	
+});
+
+
+app.post("/favorites/:imdbID", function(req, res){
+	console.log(req.body);
+	var id = req.params.imdbID;
+	
+	db.favorite.create({
+		imdbId: id, 
+		title: req.body.Title,
+		year: req.body.Year 
+	}).then(function(){
+		res.redirect("../favorites");
+		// res.render("favorites.ejs", {favorite:data});
+		// console.log(data);
+	});	
+	// res.redirect("/favorites")
+});
+
+
+
+
+
+
 
 
 
