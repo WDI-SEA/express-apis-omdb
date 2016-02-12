b86b5b1fbec0fdb86ef5755fa5fa2c9bc845ded4
cassicakes <aguiar.cas@gmail.com>
2016-02-12 08:58:21 -0800

    working homework, ugly as sin

--- omdb/index.js
+++ omdb/index.js
@@ -2,13 +2,28 @@ var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var request = require('request');
 var app = express();
+var db = require('./models'); //every model represents a table in our table base
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(express.static(__dirname + '/static'));
 
+
+
+
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/movies/favorites', function(req, res) {
+  db.favorite.findAll().then(function(favs) {
+    res.render('favPage', {
+      allFavs: favs
+    });
+
+  });
 });
 
 app.get('/movies', function(req, res) {
@@ -25,12 +40,21 @@ app.get('/movies', function(req, res) {
 });
 
 app.get('/movies/:imdbID', function(req, res) {
-  // res.send(req.params.imdbID);
   var searchQuery = req.query.q ? req.query.q : '';
   var imdbID = req.params.imdbID;
   request('http://www.omdbapi.com/?i=' + imdbID, function(err, response, body) {
     res.render('movieShow', {movie: JSON.parse(body),
                              q: searchQuery});
+  });
+});
+
+app.post('/getFavs', function(req, res) {
+  db.favorite.create({// this is called a db query
+    imdb_code: req.body.imbd_id,// req.body is taking any input and storing it, it's apart of body parser
+    title: req.body.title,
+    year: req.body.year
+  }).then(function(saved) {
+    res.redirect('/movies/favorites');///movies/favorires is url parth
   });
 });
 
