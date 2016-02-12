83d2772d85df37f9cf82311015e3b71668cf4235
katiosity <lovan.kathy@gmail.com>
2016-02-11 23:53:42 -0800

    added favorites button, favorites page, favorites controller, and db: omdb_app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var db = require("./models");
 app.set('view engine', 'ejs');
 // app.use(bodyParser.urlencoded{extended: false}));
 
@@ -12,6 +13,11 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'images')));
 
+
+var favCtrl = require('./controllers/favorites.js');
+app.use('/favorites', favCtrl);
+
+
 app.get('/', function(req, res) {
 	res.render('movieSearchForm/index.ejs');
 })
@@ -19,19 +25,22 @@ app.get('/', function(req, res) {
 app.get('/results', function(req, res) {
 	var title = req.query.title;
 	request(
-		'http://www.omdbapi.com/?s='+title, 
+		'http://www.omdbapi.com/?type=movie&s='+title, 
 		function(error, response, body) {
-			if(!error && response.statusCode == 200) {
-				var data = JSON.parse(body);
+			var data = JSON.parse(body);
+			if(!error && response.statusCode == 200 && data) {
 				res.render('movieSearchResults.ejs', {
 					movie: data
 				});
+			} else {
+				res.render('No movies found!');
 			}
 		}
 	);
 });
 
 app.get('/details/:id', function(req, res) {
+	// var searchQuery = req.query.q ? req.query.q : '';
 	var id = req.params.id;
 	request('http://www.omdbapi.com/?i='+id, 
 		function(error, response, body) {
@@ -46,7 +55,42 @@ app.get('/details/:id', function(req, res) {
 	);
 });
 
+
+// app.post('/favorites', function(req, res) {
+// 	var data = req.body;
+// 	db.omdb_app.create({
+// 		imdbID: data.imdbID,
+// 		title: data.Title,
+// 		year: data.Year,
+// 		poster: data.Poster,
+// 	}).then(function(){
+// 		res.redirect('/favorites/index.ejs');
+// 	})
+// });
+
+
+// need app.use controllers/favorites.js here
+
+
+
 var movieSearchFormCtrl = require('./controllers/movieSearchForm');
 app.use('/', movieSearchFormCtrl);
 
+
+
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ models/favorite.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var favorite = sequelize.define('favorite', {
+    imdbId: DataTypes.STRING,
+    title: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    poster: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return favorite;
+};
