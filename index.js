429c6c9826423308fefbe24fd708b806ddb134da
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-04-12 23:38:56 -0700

    Able to make super simple movie search

---
+++ index.js
@@ -0,0 +1,61 @@
+//requires
+var express = require("express");
+var request = require("request");
+
+var app = express();
+app.set('view engine', 'ejs');
+
+var ejsLayouts = require("express-ejs-layouts");
+app.use(ejsLayouts);
+
+app.use(express.static(__dirname + '/views'));
+
+//routes
+app.get("/", function(req, res) {
+  res.render("index", {});
+});
+
+app.get("/movies", function(req, res) {
+  var query = req.query.q;
+//  var pageNum = req.query.page;
+
+  if(query === '' || query === undefined){
+    res.render('index');
+    return;
+  }
+
+//  if(pageNum === '' || pageNum === undefined){
+//    pageNum = 1;
+//  }
+
+  request('http://www.omdbapi.com/?s=' + query, function(err, response, body) {
+    var data = JSON.parse(body);
+    if(!err && response.statusCode === 200 && data.Search) {
+      res.render('movies', {movies: data.Search, q: query}); //, page: pageNum, numHits: data.totalResults});
+      //res.send(data.Search);
+    }
+    else{
+      res.render('index'); // Need to add a "couln't find" page
+    }
+  });
+});
+
+app.get("/movies/:imdbID", function(req, res) {
+  var imdbIDRequested = req.params.imdbID;
+  console.log(imdbIDRequested);
+
+  request('http://www.omdbapi.com/?i=' + imdbIDRequested, function(err, response, body) {
+    var data = JSON.parse(body);
+    console.log(data.Title);
+    if(!err && response.statusCode === 200){
+      res.render('show', {showData: data});
+      //res.send(data.Title);
+      console.log("Got into the if");
+    }
+    else{
+      res.render('index'); // Need to add a "couldn't find" page
+    }
+  });
+});
+
+app.listen(3000);
