5e8681d9eaa4ca44356f55a7333393de63599a06
drewrudebusch <drewrudebusch@gmail.com>
2016-04-13 01:26:45 -0700

    Random movie on the homepage, all movie results linked to detail page, rating displayed on detail pages

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(ejsLayouts);
 app.use(express.static('static'));
 
 app.get("/", function(req, res) {
-    var searchTerms = ['ghost', 'league', 'war', 'bear', 'fun', 'pizza']
-    var randomTerm = searchTerms[Math.floor(Math.random() * 6)];
+    var searchTerms = ['ghost', 'league', 'war', 'bear', 'fun', 'pizza', 'old', 'ace', 'happy']
+    var randomTerm = searchTerms[Math.floor(Math.random() * searchTerms.length)];
     var randomMovieId = 0;
 
   request('http://www.omdbapi.com/?s=' + randomTerm + '&type=movie', function (error, response, body) {
@@ -19,7 +19,6 @@ app.get("/", function(req, res) {
 
       request('http://www.omdbapi.com/?i=' + randomMovieId + '&type=movie&plot=full', function (error, response, body) {
         var movie = JSON.parse(body);
-        console.log(movie)
         if (!error && response.statusCode == 200&& movie ) {
           res.render('index', {randomMovie: movie});
         } else {
@@ -30,22 +29,38 @@ app.get("/", function(req, res) {
       res.render('index-safe');
     }
   });
-
-  // res.render("index");
 });
 
 app.get("/movies", function(req, res) {
 	var query = req.query.q;
 
-  request('http://www.omdbapi.com/?s=' + query, function (error, response, body) {
-    var data = JSON.parse(body);
-    console.log(data)
-    if (!error && response.statusCode == 200 && data.Search) {
-      res.render("movies", {movies: data.Search, q: query});
-    } else {
-    	res.render('index');
-    }
-  });
+  if (query) {
+    request('http://www.omdbapi.com/?s=' + query, function (error, response, body) {
+      var data = JSON.parse(body);
+      console.log(data)
+      if (!error && response.statusCode == 200 && data.Search) {
+        res.render("movies", {movies: data.Search, q: query});
+      } else {
+        res.render('movies-safe');
+      }
+    });
+  } else {
+    res.render('movies-safe');
+  }
+
+});
+
+app.get('/movies/:index', function(req, res) {
+  var movieId = req.params.index;
+
+  if (movieId) {
+    request('http://www.omdbapi.com/?i=' + movieId + '&type=movie&plot=full', function (error, response, body) {
+      var data = JSON.parse(body);
+      res.render('show', {myMovie: data});
+    });
+  } else {
+    res.render('movies-safe');
+  }
 });
 
 app.listen(3000);
