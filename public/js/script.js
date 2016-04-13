5c6d1389318040b9d75d33e029ef074dc3872120
brytree <brycebortree@gmail.com>
2016-04-13 10:00:55 -0700

    yep

--- public/js/script.js
+++ public/js/script.js
@@ -3,36 +3,36 @@ $(document).ready(){
 
   switch(parseInt(movie.imdbRating)) {
       case 0 > && < 1:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-        break;
-      case 1> && <2:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-            break;
-      case 2 > && < 3:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-            break;
-      case 3 > && < 4:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-                break;
-      case 4 > && < 5:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-            break;
-      case 5 > && < 6:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-            break;
-      case 6 > && < 7:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-            break;
-      case 7 > && < 8:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-            break;
-      case 8 > && < 9:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-            break;
-      case 9 > && < 10:
-          $(#stars).prepend('<img src="./css/assets/star.gif" />');
-            break;
-      default:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //   break;
+      // case 1> && <2:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //       break;
+      // case 2 > && < 3:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //       break;
+      // case 3 > && < 4:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //           break;
+      // case 4 > && < 5:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //       break;
+      // case 5 > && < 6:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //       break;
+      // case 6 > && < 7:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //       break;
+      // case 7 > && < 8:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //       break;
+      // case 8 > && < 9:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //       break;
+      // case 9 > && < 10:
+      //     $(#stars).prepend('<img src="./css/assets/star.gif" />');
+      //       break;
+      // default:
           return}
 
 });
