3bf2982988c4877d2fe3ff5187d0caad658a7c04
derek_robertson <dwilliamrobertson@gmail.com>
2016-06-14 15:58:55 -0700

    movie search working from form submit

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+var ejsLayouts = require('express-ejs-layouts');
+app.use(ejsLayouts);
+
+app.use(require('morgan')('combined'));
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('search');
+});
+
+app.post('/search', function(req, res){
+  console.log(req.body);
+  request({
+    url: 'http://www.omdbapi.com',
+    qs: {
+      s: req.body.name
+    },
+    headers: {
+      'Accept': 'application/json'
+    }
+  }, function(error, response, body){
+    console.log(body);
+    if(!error && response.statusCode === 200){
+      var dataObj = JSON.parse(body);
+
+      res.send(dataObj.Search);
+      // res.render('results', {results: dataObj.Search});
+    }
+  });
 });
 
 var server = app.listen(process.env.PORT || 3000);
