7ca39cefc378799997126f901eda96fe7f17dc43
Brian Hague <bhague1281@gmail.com>
2016-06-15 09:47:34 -0700

    working routes. yusss

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', function(req, res) {
   res.send('Hello Backend!');
 });
 
+app.get('/results', function(req, res) {
+  res.send('This is /results');
+});
+
+app.get('/movies/:imdbId', function(req, res) {
+  res.send('This is /movies/:imdbId');
+});
+
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
