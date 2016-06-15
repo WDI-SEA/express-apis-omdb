0fc3c1e86a01093fe28c45a805c31291de5336f6
Josh McBroom <mcbroomjc@gmail.com>
2016-06-14 20:55:00 -0700

    Pushing what I have gotten done on express-apis-omdb

---
+++ controllers/movie.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+
+router.get('/:i', function(req, res) {
+  request({
+    url: 'http://www.omdbapi.com/?',
+    qs: {
+      i: req.params.i
+    }
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var movie = JSON.parse(body);
+      // res.send for now (to test) - later, res.render
+      res.send(movie);
+      // res.render('result', { title: movie.Title,
+      // year: movie.Year,
+      // rating: movie.Rated,
+      // genre: movie.Genre,
+      // director: movie.Director,
+      // cast: movie.Actors,
+      // plot: movie.Plot,
+      // imdbRating: movie.imdbRating
+      // });
+    }
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+// Taking care of standard requirements
 var express = require('express');
+var request = require('request');
 var app = express();
 
+// Sets view engine
+app.set('view engine', 'ejs');
+
+// Requires express-ejs-layouts
+var ejsLayouts = require("express-ejs-layouts");
+app.use(ejsLayouts);
+
+// Requires morgan devkit (? purpose unknown ?)
 app.use(require('morgan')('dev'));
 
+// Renders main page index.ejs
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('index');
 });
+
+// Search function takes query, searches omdb's database, renders results.ejs
+app.get('/results', function(req, res) {
+  request({
+    url: 'http://www.omdbapi.com/?',
+    qs: {
+      s: req.query.search
+    }
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var movies = JSON.parse(body);
+      // res.send(dataObj.Search);
+      res.render('results', { results: movies.Search
+      });
+    }
+  });
+});
+
+var movieCtrl = require('./controllers/movie');
+app.use('/movies', movieCtrl);
 
 var server = app.listen(process.env.PORT || 3000);
 
