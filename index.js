fba687e5f3c8e728a205609437ebabbe50f4328b
Brian Hague <bhague1281@gmail.com>
2016-06-15 10:04:40 -0700

    views working, chyeahh

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 
+app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
+app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('main.ejs');
 });
 
 app.get('/results', function(req, res) {
