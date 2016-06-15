914540f87ef8c5400155eb29748af6601b6d8ee5
Derek Robertson <dwilliamrobertson@gmail.com>
2016-06-14 23:50:47 -0700

    wired up and styled

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
+var prevSearch;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 var ejsLayouts = require('express-ejs-layouts');
 app.use(ejsLayouts);
+
+app.use(express.static(__dirname + '/public'));
 
 app.use(require('morgan')('combined'));
 
@@ -15,7 +18,7 @@ app.get('/', function(req, res) {
 });
 
 app.post('/search', function(req, res){
-  console.log(req.body);
+  prevSearch = req.body.name;
   request({
     url: 'http://www.omdbapi.com',
     qs: {
@@ -30,10 +33,28 @@ app.post('/search', function(req, res){
       var dataObj = JSON.parse(body);
 
       // res.send(dataObj.Search);
-      res.render('results', {results: dataObj.Search});
+      res.render('results', {results: dataObj.Search, queryString: req.body.name});
     }
   });
 });
+
+app.get('/info/:imdbID', function(req, res){
+  request({
+    url: 'http://www.omdbapi.com',
+    qs: {
+      i: req.params.imdbID
+    },
+    headers: {
+      'Accept': 'application/json'
+    }
+  }, function(error, response, body){
+    if(!error && response.statusCode === 200){
+      var dataObj = JSON.parse(body);
+      res.render('info', {info: dataObj, prevSearch: prevSearch});
+    }
+  });
+});
+
 
 var server = app.listen(process.env.PORT || 3000);
 
