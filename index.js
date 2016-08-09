aadd7ff13e3e455cd1c3268dbe05acf496cf000d
zmcdaniel <zmcdaniel@gmail.com>
2016-08-09 16:06:02 -0700

    working form querires omdb and returns all movies matching query string

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+// REQUIREMENTS
 var express = require('express');
+var request = require('request');
+var ejsLayouts = require('express-ejs-layouts');
+
+// GLOBAL VARIABLES
 var app = express();
 
+// SET/USE STATEMENTS
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
+// ROUTES
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('movies/index');
 });
 
+app.get('/results', function(req, res){
+  var qs = {
+    s: req.query['q'],
+    plot: 'short',
+    r: 'json'
+  }
+  request({
+    url: 'http://www.omdbapi.com/',
+    qs: qs
+  }, function(error, respose, body){
+    var data = JSON.parse(body);
+    res.send(data.Search);
+  });
+});
+
+// LISTEN
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
