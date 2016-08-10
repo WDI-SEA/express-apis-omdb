067a7c7c6eb9839ec3adc44863d3d6faedc89b03
teemopower <jimmykim925@gmail.com>
2016-08-09 22:54:40 -0700

    updated solution

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
 
-// HOME PAGE
+/////////////////// HOME PAGE
 app.get("/", function(req,res){
   res.render("index");
 });
 
-// RESULTS
+/////////////////// RESULTS
 app.get("/results", function(req, res){
   var fileContents = fs.readFileSync('data.json');
   var data = JSON.parse(fileContents);
@@ -22,26 +22,59 @@ app.get("/results", function(req, res){
     s: data.title
     //r: 'json'
   }
-
   request({
     url:'http://www.omdbapi.com/',
     qs: qs
   }, function(error, response, body){
     var dataObj = JSON.parse(body);
-    
     res.render("results", {results: dataObj.Search});
-  });
-  
+  }); 
 });
 
-// POST 
+//////////////////// POST 
 app.post('/results', function(req,res){
   //console.log(req.body);
-
   fs.writeFileSync('data.json', JSON.stringify(req.body));
-  
   res.redirect('/results');
 });
 
+//////////////////// MOVIES
+app.get('/movies/:imdbID', function(req, res){
+  //res.render('movies', {results: dataObj[parseInt(req.params.imdbId)]});
+  console.log("parse " + req.params.imdbID);
+
+  var qs = {
+    i: req.params.imdbID
+    //r: 'json'
+  }
+  request({
+    url:'http://www.omdbapi.com/',
+    qs: qs
+  }, function(error, response, body){
+     var data = JSON.parse(body);
+     console.log({myMovies: data.Plot});
+    res.render("movies", {myMovies: data.Plot});
+  }); 
+
+});
+
+////////////////// Listener
 var server = app.listen(process.env.PORT || 3000);
 module.exports = server;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
