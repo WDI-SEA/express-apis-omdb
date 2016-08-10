6683aed5998cce67399775a7c32f3578efab19f2
Boet Waldbjorn <boetw@me.com>
2016-08-10 10:47:58 -0700

    updated

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ var express = require('express');
 var app = express();
 var ejsLayouts = require('express-ejs-layouts');
 var request = require('request')
-var fs = require("fs");
-var bodyParser = require("body-parser");
 
 
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
-app.use(bodyParser.urlencoded({extended: false}));
 
 
 
@@ -45,17 +42,15 @@ app.get('/results', function(req, res) {
 		url: 'http://www.omdbapi.com',
 		qs: qs
 	}, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+
 		      var dataObj = JSON.parse(body);
 
 			res.render("site/results", {results: dataObj.Search});
+		}
 	});
 });
 
-app.post('/results', function(req,res){
-   //console.log(req.body);
-   fs.writeFileSync('data.json', JSON.stringify(req.body));
-   res.redirect('/results');
- });
 
 app.get('/movie/:imdbID', function(req, res){
  
@@ -69,9 +64,14 @@ app.get('/movie/:imdbID', function(req, res){
      url:'http://www.omdbapi.com/',
      qs: qs
    }, function(error, response, body){
+   			if (!error && response.statusCode == 200) {
+
       var data = JSON.parse(body);
-      console.log({myMovies: data.Plot});
+      console.log(data);
      res.send(dataObj);
+     res.render("movie", {movieData: data});
+   }
+
    }); 
  
  });
