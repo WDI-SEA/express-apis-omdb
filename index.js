a8f17036ba51f1072979bc9446622de22d261c0f
shaneehlert <shane@esperas.org>
2016-08-10 09:27:07 -0700

    just dont have detailed view of the movies that come up

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
+'use strict'; //enable let
+
+// DEPENDENCIES
+
 var express = require('express');
 var app = express();
+var ejs = require('ejs');
+var request = require('request');
 
-app.use(require('morgan')('dev'));
 
-app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+var DEFAULT_PORT = 3000;
+var PORT = process.env.PORT || DEFAULT_PORT;
+var SERVER_MSG = 'Api Test is now running on port ' + PORT;
+var mediaTypeStrings = {
+  '': 'Media',
+  'movie': 'Movies',
+};
+
+
+
+app.set('view engine', 'ejs');
+
+
+
+// SERVER
+
+app.listen(PORT, function() {
+  console.log(SERVER_MSG);
 });
 
-var server = app.listen(process.env.PORT || 3000);
+// ROUTES
 
-module.exports = server;
+app.get('/', function(req, res) {
+  res.render('search');
+});
+
+app.get('/results', function(req, res) {
+  var searchTerm = req.query.searchTerm;
+  var mediaType = req.query.mediaType;
+  var searchUrl = `http://www.omdbapi.com/?s=${searchTerm}&type=${mediaType}`;
+
+
+  request(searchUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var searchResults = JSON.parse(body);
+
+
+      res.render('results', {
+        searchTerm: searchTerm,
+        mediaType: mediaTypeStrings[mediaType],
+        searchResults: searchResults.Search
+      });
+    } else {
+      console.log('Status Code: ', response.statusCode);
+      console.log('Error: ', error);
+    }
+  });
+});
+
+// MAIN
