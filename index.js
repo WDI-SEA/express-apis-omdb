a82b00b158884d32fe3d7776d3572d7dea70ccde
zmcdaniel <zmcdaniel@gmail.com>
2016-08-18 13:34:40 -0700

    pushin

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var request = require('request');
 var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 // GLOBAL VARIABLES
 var app = express();
@@ -10,6 +12,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // ROUTES
 app.get('/', function(req, res) {
@@ -39,9 +42,21 @@ app.get('/movie/:imdbId', function(req, res){
     qs: qs
   }, function(error, response, body) {
     var data = JSON.parse(body);
-    console.log(data);
+    //console.log(data);
     res.render('movies/show', {movieData: data});
   });
+});
+
+app.get('/movie/favorites', function(req, res) {
+  var jsonData = JSON.parse(fs.readFileSync('./favorites.json', 'utf8'));
+  res.render('favorites', {favorites: jsonData.favorites});
+});
+
+app.post('movies/favorites', function(req, res){
+  var jsonData = JSON.parse(fs.readFileSync('./favorites.json', 'utf8'));
+  jsonData.favorites.push(req.body);
+  fs.writeFileSync('./favorites.json', JSON.stringify(jsonData));
+  res.redirect('/favorites');
 });
 
 // LISTEN
