dbdb12eb4125828ee7ab5a14b84afa3dd1311bf2
hayes9321 <t.hayes8321@gmail.com>
2016-10-05 08:27:49 -0700

    finished assignment

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
 var express = require('express');
+var request = require('request');
+var bodyParser = require("body-parser");
+var fs = require("fs");
 var app = express();
 
-app.use(require('morgan')('dev'));
+//app.use(require('morgan')('dev'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+/////////////////// HOME PAGE
+app.get("/", function(req,res){
+  res.render("index");
 });
 
-var server = app.listen(process.env.PORT || 3000);
+/////////////////// RESULTS
+app.get("/results", function(req, res){
+  var fileContents = fs.readFileSync('data.json');
+  var data = JSON.parse(fileContents);
+  console.log(data.title);
+  var qs = {
+    s: data.title
+    //r: 'json'
+  }
+  request({
+    url:'http://www.omdbapi.com/',
+    qs: qs
+  }, function(error, response, body){
+    var dataObj = JSON.parse(body);
+    res.render("results", {results: dataObj.Search});
+  }); 
+});
 
+//////////////////// POST 
+app.post('/results', function(req,res){
+  //console.log(req.body);
+  fs.writeFileSync('data.json', JSON.stringify(req.body));
+  res.redirect('/results');
+});
+
+//////////////////// MOVIES
+app.get('/movies/:imdbID', function(req, res){
+  //res.render('movies', {results: dataObj[parseInt(req.params.imdbId)]});
+  console.log("parse " + req.params.imdbID);
+
+  var qs = {
+    i: req.params.imdbID
+    //r: 'json'
+  }
+  request({
+    url:'http://www.omdbapi.com/',
+    qs: qs
+  }, function(error, response, body){
+     var data = JSON.parse(body);
+     console.log({myMovies: data.Plot});
+    res.render("movies", {myMovies: data.Plot});
+  }); 
+
+});
+
+////////////////// Listener
+var server = app.listen(process.env.PORT || 3000);
 module.exports = server;
