26f0d729eac0948c60e7fc7d0c2e5b08bb22d75f
jedimasterclos <carlossrinivas@gmail.com>
2016-10-05 08:59:01 -0700

    Carlos: API OMDB

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+
 var app = express();
 
+
+
+
 app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('index.ejs');
 });
+
+app.get('/results', function(req, res) {
+	var searchInput = {
+		s: req.query.searchInput
+	};
+	request({
+		url: "http://www.omdbapi.com",
+		qs: searchInput
+	},
+	function(error, response, body) {
+		if(!error && response.statusCode == 200) {
+			var movieData = JSON.parse(body);
+			// res.send(movieData.Search);
+			res.render('./results', { movies: movieData.Search })
+		}
+		else {
+			res.send('An error happened ' + error);
+		}
+	});
+});
+
+app.get('/movies/:imdb', function(req, res) {
+	var imdbID = {
+		i: req.params.imdb
+	};
+	request({
+		url: "http://www.omdbapi.com",
+		qs: imdbID
+		
+		
+	}, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var movieData = JSON.parse(body);
+			res.render('./movies.ejs', { movie: movieData });
+		} else {
+			res.send('This is not a real movie');
+		}
+	});	
+
+});
+
 
 var server = app.listen(process.env.PORT || 3000);
 
