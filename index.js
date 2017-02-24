3372f44bd76b5f326ef8eba85efd88eb63d0856a
m-woo <mark.chi.woo@gmail.com>
2017-02-24 00:30:03 -0800

    mark-express-film-search

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
+//require global vars
 var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var request = require('request');
 var app = express();
 
+
+//set and use statements
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
 app.use(require('morgan')('dev'));
 
-app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+app.get('/', function(req, res){
+	res.render('search')
 });
 
-var server = app.listen(process.env.PORT || 3000);
+app.get('/results', function(req, res) {
+	// var search = req.query.name
+	var qs = {
+		s: req.query.name
+	};
+	request({
+		url:'http://www.omdbapi.com',
+		qs: qs
+	}, function(error, response, body){
+		if (!error && response.statusCode == 200){
+			var dataObj = JSON.parse(body);
+			res.render('results', {results: dataObj.Search});
+		}
+	});
+});
 
+
+
+app.get('/movies/:id', function(req, res){
+		var id = req.params.id
+		var qs = {
+		i: id
+	};
+	request({
+		url:'http://www.omdbapi.com',
+		qs: qs
+	}, function(error, response, body){
+		if (!error && response.statusCode == 200){
+			var dataObj = JSON.parse(body);
+			res.render('movies', {movies: dataObj});
+		}
+	});
+});
+
+
+
+var server = app.listen(process.env.PORT || 3000);
 module.exports = server;
+
