154c37ddcabb73a4b693c129fbc756e6ffeaa5d9
jnhustin <jnhustin@gmail.com>
2017-02-24 07:19:05 -0800

    completed 3/4 requirements, did not implement controller

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
+// require statements & global variables
 var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayout = require('express-ejs-layouts');
+var request = require('request');
 var app = express();
 
-app.use(require('morgan')('dev'));
 
+// set and use statements
+app.set('view engine', 'ejs');
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded( {extended:false} ));
+app.use(ejsLayout);
+
+//app.use('/movies', require('/controllers/movies'));
+
+// routes
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+	res.render('home'); // render a view
+});
+app.get('/search', function(req, res){
+	var s = req.query.search;
+	console.log(s);
+	request ({
+		url: 'http://www.omdbapi.com/',
+		qs: {s: s},
+		r: 'json'
+	}, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var movieData = JSON.parse(body)['Search'];
+			// console.log(movieData);
+			res.render('results', { movieData: movieData } );
+		}
+	});
+});
+app.get('/movie/:movieId', function(req, res) {
+	// console.log(req.params);
+	var i = req.params['movieId'];
+
+	request ({
+		url: 'http://www.omdbapi.com/',
+		qs: {i: i},
+		r: 'json'
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var movieData = JSON.parse(body);
+			// console.log(movieData)
+			res.render('id', { movieData:movieData });
+		}
+	});
 });
 
-var server = app.listen(process.env.PORT || 3000);
 
+
+
+// listen
+var server = app.listen(process.env.PORT || 3000);
 module.exports = server;
