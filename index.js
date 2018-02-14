89392edb0ae3f0726085177f3f400df530dbd923
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-02-13 21:49:15 -0800

    index search results boiler plate

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
+// OMDB Search Site - APIs and Express
 var express = require('express');
 var app = express();
 var request = require('request');
-var bodyParser = require("body-parser");
-require("dotenv").config();
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:false}));
+var bodyParser = require('body-parser');
+var path = require('path');
+require('dotenv').config();
+app.set('view engine', 'ejs');
 
 // this adds some logging to each request
 app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
 
-// app.get('/', function(req, res) {
-//     res.send('Hello Backend!');
-// });
 
-app.get("/", function(req,res){
+
+
+// ROUTES
+app.get('/', function(req,res){
   console.log('whawtup');
-  // var qs = {
-  //   s:"star wars"
-  // };
-  request('http://www.omdbapi.com/?apikey=' + process.env.OMDB_KEY + '&s=star wars', function(error,response,body) {
-    console.log(response.statusCode);
+  var qs = {
+    s:'star wars'
+  };
+  request({
+    url: 'http://www.omdbapi.com/?apikey=' + process.env.OMDB_KEY + '&',
+    qs: qs
+  },
+   function(error,response,body) {
+    // console.log(response.statusCode);
     if (!error && response.statusCode === 200) {
       var dataObj = JSON.parse(body);
       // console.log(dataObj);
@@ -28,29 +34,29 @@ app.get("/", function(req,res){
   });
 });
 
-app.get("/search", function(req,res){
-  // console.log("in search path");
-  res.render("search");
+app.get('/search', function(req,res){
+  // console.log('in search path');
+  res.render('search');
 });
 
-app.get("/results", function(req,res){
-  console.log("in results path");
+app.get('/results', function(req,res){
+  // console.log('in results path');
   var qs = {
     s:req.query.title
   }
   request({
-    url: "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&",
+    url: 'http://www.omdbapi.com/?apikey=' + process.env.OMDB_KEY + '&',
     qs: qs
   }, function(error, response, body){
     if (!error && response.statusCode === 200){
       var dataObj = JSON.parse(body);
       console.log(dataObj);
-      // var movieTitle = req.params.title;
-      res.render("results", {movies: dataObj.Search});
+      res.render('results', {movies: dataObj.Search});
     }
   })
 });
 
+// listen
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
