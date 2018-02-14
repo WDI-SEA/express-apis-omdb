1cc60861c013bfdfb77c5d9e46423ea8ba76bb82
Daniel Heyward <danheyward@gmail.com>
2018-02-13 16:07:03 -0800

    functionality is set

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+require('dotenv').config();
 
-// this adds some logging to each request
-app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs');
+
 
 app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+  res.render('index');
+})
+
+
+app.get('/results', function(req, res) { // hit the /results route
+  var qs = {
+    s: req.query.title
+  };
+  request({
+    url: 'http://www.omdbapi.com/?apikey=' + process.env.OMDB_KEY + '&',
+    qs: qs
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var dataObj = JSON.parse(body);
+      res.render('results', { movies: dataObj.Search }); // change index to whatver your ejs file is called
+    }
+  });
 });
 
-var server = app.listen(process.env.PORT || 3000);
 
-module.exports = server;
+app.get('/movies/:movie_id', function(req, res) {
+  var qs = {
+    i: req.params.movie_id
+  };
+  request({
+    url: 'http://www.omdbapi.com/?apikey=' + process.env.OMDB_KEY + '&',
+    qs: qs
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var dataObj = JSON.parse(body);
+      res.render('movie', { movies: dataObj }); // change index to whatver your ejs file is called
+    }
+  });
+});
+
+
+
+
+
+
+
+app.listen(3000);
