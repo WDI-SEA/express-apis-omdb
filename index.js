64a0d97388c0ee34d19db1e94c229cc53cf3e70b
Brett <brettspencr@gmail.com>
2018-02-13 17:06:56 -0800

    Finished the deliverable... but with that weird bug still

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+var path = require("path");
+
 var app = express();
 
-// this adds some logging to each request
-app.use(require('morgan')('dev'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(express.static(path.join(__dirname, "static")));
 
-app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+app.use(express.static(__dirname + '/static/'));
+
+app.get("/", function(req, res) {
+    res.render("search");
 });
 
-var server = app.listen(process.env.PORT || 3000);
+app.post("/results", function(req, res){
+    console.log("In the /results route");
+    var title = req.body.title;
+    var qs = {
+        s: title
+    }
+    request({
+        url: "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&", 
+        qs: qs
+    }, function(error, response, body){
+        if (!error && response.statusCode === 200) {
+            var dataObj = JSON.parse(body);
+            console.log(dataObj.Search);
+            res.render("results", { movies: dataObj.Search });
+        }  
+    });
+});
 
-module.exports = server;
+app.get("/movie/:id", function(req, res){
+    console.log("In the /results/:id route...");
+    request({
+        url: "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&i=" + req.params.id, 
+    }, function(error, response, body){
+        if (!error && response.statusCode === 200) {
+            var dataObj = JSON.parse(body);
+            console.log(dataObj);
+            res.render("movie", { movie: dataObj }); // change index to whatever your ejs file is called
+        }  
+    });
+});
+
+
+
+var server = app.listen(3000);
+
+// module.exports = server;
