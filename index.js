7083c3eede5d919afa113b19fb4bdf46646a6f58
Chelsea Zhu <qianwei5668@gmail.com>
2018-02-13 16:29:53 -0800

    post movie function complete

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 var express = require('express');
-var app = express();
+var request = require('request'); 
+var bodyParser = require('body-parser');
 
-// this adds some logging to each request
-app.use(require('morgan')('dev'));
+require('dotenv').config(); //this node module is just written this way, env way --then process.env.OMDB_KEY
+
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.send('Hello Backend!');
-});
+	res.render('index');
+})
 
-var server = app.listen(process.env.PORT || 3000);
+app.post("/results", function(req, res) {
+	var qs = {
+		s: req.body.title //we put s as the property b/c OMDB requires it
+	}
+	request({
+		url: "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&",
+		qs: qs
+	}, function (error, response, body) {
+		console.log(error, response);
+		if (!error && response.statusCode === 200) {
+			var dataObj = JSON.parse(body);
+			console.log('dataObj');
+			res.render('results', {movies: dataObj.Search})
+		}
+	}
+  )
+})
 
-module.exports = server;
+
+
+
+app.listen(3000);
