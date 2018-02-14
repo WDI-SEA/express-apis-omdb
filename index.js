e501f8f01413322ab539d7bfb98a9d5a1fb8e0cc
Michael Hammer <michaelhammer@Michaels-MacBook-Pro.local>
2018-02-14 08:50:37 -0800

    Michael H exp-movie-api

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 var express = require('express');
+var request = require('request');
+// var bodyParser = require('body-parser');
 var app = express();
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({extended: false}))
 
 // this adds some logging to each request
-app.use(require('morgan')('dev'));
+// app.use(require('morgan')('dev'));
 
 app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+    res.render('index');
 });
 
-var server = app.listen(process.env.PORT || 3000);
 
-module.exports = server;
+app.get('/results', function(req, res) { //hit the /results route
+  console.log("im in the route");
+  var qs = {
+    s: req.query.title //req.query.title
+  };
+  request({
+    url: 'http://www.omdbapi.com/?apikey=' + process.env.OMDB_KEY + '&',
+    qs: qs
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var dataObj = JSON.parse(body);
+      console.log(dataObj);
+      res.render('results', {movies: dataObj.Search}); //change index to whatever your ejs file is called
+    }
+  })
+});
+
+app.get('/movie/:movie_id', function(req, res) { //hit the /results route
+  console.log("im in the route");
+  var qs = {
+    i: req.params.movie_id //req.query.title
+  };
+  request({
+    url: 'http://www.omdbapi.com/?apikey=' + process.env.OMDB_KEY + '&',
+    qs: qs
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var dataObj = JSON.parse(body);
+      console.log(dataObj);
+      res.render('movie', {movie: dataObj}); //change index to whatever your ejs file is called
+    }
+  })
+});
+
+
+app.listen(3000);
+
+// var server = app.listen(process.env.PORT || 3000);
+//
+// module.exports = server;
