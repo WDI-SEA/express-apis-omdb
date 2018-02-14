284dd1099f1f839087cd778c37c51ef51c7c042f
Kyle Van Bergen <kyleavb@gmail.com>
2018-02-13 17:36:53 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-var express = require('express');
+var express = require("express");
+var request = require("request");
+require("dotenv").config();
+
 var app = express();
+app.set("view engine", "ejs");
 
-// this adds some logging to each request
-app.use(require('morgan')('dev'));
-
-app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+app.get("/results", function(req, res){
+  console.log("running results");
+  var qs = {
+    s:req.query.search
+  };
+  console.log(qs);
+  request({
+    url: "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&",
+    qs: qs
+  }, function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var dataObj = JSON.parse(body);
+      console.log(dataObj);
+      res.render("show", {movies: dataObj.Search});
+    };
+  });
 });
 
-var server = app.listen(process.env.PORT || 3000);
+app.get("/movies/:movie_id", function(req, res){
+  var movieID = req.params.movie_id;
+  var qs = {
+    i: movieID
+  };
+  request({
+    url: "http://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY + "&",
+    qs: qs
+  }, function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var details = JSON.parse(body);
+      console.log(details);
+      res.render("specific", {movieDetail: details});
+    };
+  })
+});
 
-module.exports = server;
+app.get('*', function(req, res) {
+    res.render("index");
+});
+app.listen(5000);
