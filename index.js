dbc146fffe1aa5f540e43d6ca9e8afdead26f30d
yuanjli <liyuanjienetwork@gmail.com>
2018-08-15 07:39:44 -0700

    added request function to the index.js

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ var app = express();
 // this adds some logging to each request
 app.use(require('morgan')('dev'));
 
-app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+$('#btnSearch').on('click', function () {
+	var searchInput = $('#textBoxElement').val();	
+	app.get('/results', function(req, res) {
+		var url = "http://monkey=" + searchInput + "&red";
+		request(url, function(err, response, body){
+		if(!err && response.statusCode === 200){
+			var parsedJson = JSON.parse(body);
+			res.render('home', {movies: parsedJson.Search});
+		}
+		else {
+			console.log(err);
+			res.send('error');
+		}
+		res.send('Hello Backend!');
+	});
+    // var url = "http://monkey=" + searchInput + "&red";
+    // window.open(url);
 });
+
+
+
+
 
 var server = app.listen(process.env.PORT || 3000);
 
