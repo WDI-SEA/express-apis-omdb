edbf8c4a2e8b810bd8ba51457f1d14cee19e12c2
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-14 19:46:27 -0700

    middleware layout set up. user able to search movies and return results with movie name, year and poster

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+// Require needed modules
 var express = require('express');
-var app = express();
+var expressEjsLayouts = require('express-ejs-layouts');
+var request = require('request');
+
+// Declare a new express app
+var app = express(); 
+
+// Tell express what view engine we want to use
+app.set('view engine', 'ejs'); 
+
+// Define middleware settings
+app.use(expressEjsLayouts);
 
 // this adds some logging to each request
 app.use(require('morgan')('dev'));
 
 app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+	res.render('home');
+
+});
+
+app.get('/results', function(req, res) {
+    var query = req.query.results;
+	console.log("query is ", req.query.results);
+    	request('http://www.omdbapi.com?apikey=fa0e598d&s=' + query, function(err, response, body){
+		if(!err && response.statusCode===200){
+			var parsedJson = JSON.parse(body);
+			console.log(parsedJson.Search)
+			res.render('results', {movies: parsedJson.Search});
+		}
+		else{
+			res.send(err);
+		}
+	})
 });
 
 var server = app.listen(process.env.PORT || 3000);
