3d5a6ff59dd5baf147a8d815f4af5cee6e3c18a3
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-15 08:35:03 -0700

    Added search bar using bootstrap along with some basic HTML, created search bar and passed search info into request function to allow for user search, pushed outcome to results page, still needs to retrieve imdb api

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+//Requires
 var express = require('express');
+var expressEjsLayouts = require('express-ejs-layouts')
+var request = require('request');
+
+//Variables
 var app = express();
+var apiKey = 'http://www.omdbapi.com/?apikey=34d9bb79&s='
+
+//Tell express what view engine we want to use
+app.set('view engine', 'ejs');
 
 // this adds some logging to each request
-app.use(require('morgan')('dev'));
+app.use(require('morgan')('dev')); //<------Not sure what this does
+app.use(expressEjsLayouts);
 
+//routes
 app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+    res.render('home');
 });
 
+app.get('/results', function(req, res) {
+	mySearch = req.query.search;
+	request( apiKey + mySearch, function(err, response, body) {  
+    if(!err && response.statusCode === 200){
+			var parsedJson = JSON.parse(body)
+			res.render('results', {movie: parsedJson.Search})
+		}
+		else {
+			console.log('error', err)
+			res.send(err)
+		}	
+	});
+})
+
+//listen
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
