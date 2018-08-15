d107b701b45e00b20e63a736dd303256725fb8c9
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-08-15 08:31:54 -0700

    Search query tied to api.  Results display.

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
+// Require needed modules.
 var express = require('express');
+var expressEjsLayouts = require('express-ejs-layouts');
+var request = require('request');
+
+// Declare a new express app.
 var app = express();
 
-// this adds some logging to each request
-app.use(require('morgan')('dev'));
+// Tell express what view engine we want to use.
+app.set('view engine', 'ejs');
 
+// Define middleware
+app.use(expressEjsLayouts);
+
+// Define API path
+var path = 'http://www.omdbapi.com/?i=tt3896198&apikey=2725be31&s=';
+var queryString = '';
+
+// Define routes.
+// (Route, callbackFunction(request, response))
 app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+  res.render('home');
 });
 
-var server = app.listen(process.env.PORT || 3000);
+app.get('/results', function(req, res) {
+  queryString = req.query.search_thingie;
+  console.log(queryString);
+  request(path + queryString, function(err, response, body) {
+    if(!err && response.statusCode === 200) {
+      var parsedJson = JSON.parse(body);
+      res.render('results', { movies: parsedJson.Search });
 
-module.exports = server;
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// Listen on port 3000.
+app.listen(3000);
