7b8e306f670515e9cd426f525435b7ebbc500386
mac <mac@macs-MacBook-Air.local>
2020-08-06 23:02:35 -0700

    [ejs/js/css] add routes to js, views, css to layout

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
+
+let API_KEY = process.env.API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,8 +20,48 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
 });
+
+app.get('/results', (req, res) => {
+  let qs = {
+    params: {
+        s: req.query.titleFilter,
+        apikey: API_KEY
+    }
+  }
+  axios.get('http://www.omdbapi.com', qs)
+  .then((response) => {
+      console.log(response.data)
+      let results = response.data.Search;
+      // setting variable to our data
+      res.render('results', {movies: results});
+      // render home with the data
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  let qs = {
+    params: {
+      i: req.params.movie_id,
+      apikey: API_KEY
+    }
+  }
+
+  axios.get('http://www.omdbapi.com', qs)
+  .then((response) => {
+    let results = response.data;
+    console.log(response.data)
+    res.render('detail', {movies: results})
+  })
+  .catch(err => {
+    console.log('err')
+  })
+
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
