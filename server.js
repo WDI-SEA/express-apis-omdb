e564f2760ddd905f3f4e1dc90f0cd489b849cef1
imjchiang <jchiang296@gmail.com>
2020-08-06 23:24:39 -0700

    [JS] change quotes to ones I prefer

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
+require("dotenv").config();
+const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
 const app = express();
 
 // Sets EJS as the view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // Specifies the location of the static assets folder
-app.use(express.static('static'));
+app.use(express.static("static"));
 // Sets up body-parser for parsing form data
 app.use(express.urlencoded({ extended: false }));
 // Enables EJS Layouts middleware
@@ -16,11 +16,12 @@ app.use(ejsLayouts);
 let API_KEY = process.env.API_KEY;
 
 // Adds some logging to each request
-app.use(require('morgan')('dev'));
+app.use(require("morgan")("dev"));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+app.get('/', function(req, res) 
+{
+  res.sendFile("index");
 });
 
 // The app.listen function returns a server handle
