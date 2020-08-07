7d942f4f6f7aa438ebc2785dd23137df809a23aa
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-07 01:43:51 -0700

    [express apis] finish results page but not detail page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios');
 const app = express();
 
 // Sets EJS as the view engine
@@ -16,9 +17,44 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/results', (req, res) => {
+  let qs = {
+      params: {
+          s: req.query.id,
+          apikey: '1e9edcda'
+      }
+  }
+  axios.get('http://www.omdbapi.com', qs)
+  .then((response) => {
+      console.log(response.data)
+      let movies = response.data.Search
+      res.render('results', {movies})
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
+app.get('/movies/:id', (req, res) => {
+  let qs = {
+    params: {
+        s: req.params.id,
+        apikey: '1e9edcda'
+    }
+}
+axios.get('http://www.omdbapi.com', qs)
+.then((response) => {
+  let movieData = response.data.Search
+    res.render('detail', {movieData})
+})
+.catch(err => {
+    console.log(err)
+})
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
