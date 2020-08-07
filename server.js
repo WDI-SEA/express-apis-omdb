cd1377e7b441d1d39a82cbda90efbe36d2c94d26
Lizz <lizwesterband@gmail.com>
2020-08-06 21:59:42 -0700

    [add] Add starter pages and code

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require("axios");
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+let API_KEY = process.env.API_KEY;
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,9 +18,52 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+app.get('/',(req, res)=> {
+  // res.send('Hello, backend!');
+  res.render('index');
 });
+
+app.get('/results', (req, res) => {
+  let qs = {
+      params: {
+          s: req.query.q,
+          apikey: API_KEY
+      }
+  }
+
+  axios.get('http://www.omdbapi.com', qs)
+  .then((response) => {
+    
+      let results = response.data.Search;
+      console.log(results);
+      res.render('results', {movies: results})
+      // console.log(matches)
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
+app.get('/detail/:movie_id', (req, res) => {
+  let imdbID = req.params.id
+  let qs = {
+      params: {
+          i:imdbID,
+          apikey: API_KEY
+      }
+  }
+
+  axios.get('http://www.omdbapi.com', qs)
+  .then((response) => {
+    let movieData = response.data
+    console.log(movieData)
+    res.render('detail', {movies: movieData})
+    // console.log(movieData)
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
