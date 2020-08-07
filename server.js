e40df3ffd825e209b395210b279ec8c9d7a02b3d
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-08-06 21:34:46 -0700

    results

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
-
+const axios = require('axios')
+let API_KEY = process.env.API_KEY
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
@@ -17,11 +18,35 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index')
 });
 
+
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
 
 // We can export this server to other servers like this
 module.exports = server;
+
+app.get('/results', (req, res) => {
+  let search = req.query.q
+  let qs= {
+    params: {
+      s: search,
+      apikey: API_KEY
+      
+    }
+  }
+  
+  axios.get('http://www.omdbapi.com', qs)
+  .then((response)=>{
+      console.log(response.data)
+      // let results = response.data.Search
+      res.render('results', {data:response.data.Search})
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  })
+  
+  
+  var server = app.listen(process.env.PORT || 3000);
