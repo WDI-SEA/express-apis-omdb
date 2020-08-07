565ddb40e3c17464c02fd7c0e40ae6b0132545b5
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-07 10:29:43 -0700

    [fixes] Go over deliverable and fixes gets

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const axios = require('axios');
-let API_KEY = process.env.API_KEY
-
+const API_KEY = process.env.API_KEY;
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
@@ -13,40 +12,49 @@ app.use(express.static('static'));
 app.use(express.urlencoded({ extended: false }));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
-
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
-
 // Routes
-
 app.get('/', (req, res) => {
-  res.render('index')
-})
-
+  // res.send('Hello, backend!');
+  res.render('index');
+});
 app.get('/results', (req, res) => {
-  let searchTerm = req.query.searchTerm;
+  let search = req.query.q;
   let qs = {
-      params: {
-          s: searchTerm,
-          apikey: API_KEY
-      } 
-  }
-  axios.get('http://www.omdbapi.com', qs)
+    params: {
+      s: search,
+      apiKey: API_KEY
+    }
+  };
+  axios.get(`http://www.omdbapi.com`, qs)
   .then((response) => {
-      let movie = response.data;
-      let movieData = JSON.parse(movie);
-      console.log(movie);
-      res.render(movieData);
+    let movies = response.data.Search;
+    console.log(movies);
+    res.render('results', { data: movies });
   })
   .catch(err => {
-      console.log(err)
+    console.log('Error', err);
+  });
+});
+app.get('/movies/:movie_id', (req, res) => {
+  let imdbId = req.params.movie_id;
+  let qs = {
+    params: {
+      i: imdbId,
+      apiKey: API_KEY
+    }
+  };
+  axios.get('https://www.omdbapi.com', qs)
+  .then(response => {
+    let movieData = response.data;
+    res.render('detail', { data: movieData });
   })
-})
-
-
-
+  .catch(error => {
+    console.log('Error', error);
+  });
+});
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
-
+const server = app.listen(process.env.PORT || 3000);
 // We can export this server to other servers like this
 module.exports = server;
