6d6d3a294de3003d9d940949537f9c94663b3aba
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-08-07 02:51:13 -0700

    [express-apis] movie nodes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
-// Sets EJS as the view engine
+let API_KEY = process.env.API_KEY
+// Sets EJS as the view engineNOD
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
 app.use(express.static('static'));
@@ -17,8 +19,43 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
 });
+app.get('/results', (req, res) => {
+
+  let qs = {
+    params: {
+      s: req.query.movieTitle,
+      apikey: API_KEY
+    }
+  }
+  axios.get('http://www.omdbapi.com',qs)
+  .then((response) => {
+    let search = response.data.Search
+    res.render('results', {data:search})
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+app.get('/movies/:movie_id', (req, res) => {
+  let qs = {
+    params: {
+      i: req.params.movie_id,
+      apikey: API_KEY
+    }
+  }
+  axios.get('http://www.omdbapi.com',qs)
+  .then((response) => {
+    let search = response.data
+    console.log(search)
+    res.render('detail', {data:search})
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
