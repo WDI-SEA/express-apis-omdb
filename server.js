b2e0f817339cde52a1124fde836c137f54e5a849
subrataroy321 <subrata.r321@gmail.com>
2020-08-07 13:26:58 -0400

    [SOLUTIONS] add solutions from rewiew getting hard time

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios');
 const app = express();
+
+let API_KEY = process.env.API_KEY;
+//USING .env to hide our API key
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -15,9 +19,49 @@ app.use(ejsLayouts);
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
 
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+app.get('/results', function(req, res) {
+  let q = req.query.q
+  console.log('here is movieSearch '+q);
+  let qs = {
+    params: {
+      s: q,
+      apikey: API_KEY
+    }
+  }
+  if(q) {
+    axios.get('http://www.omdbapi.com', qs)
+    .then((response) => {
+      // console.log(response.data)
+      let episodes = response.data.Search;
+      res.render('results', {episodes});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+});
+
+app.get('/movies/:movie_id', (req, res) => {
+  let imdbId = req.params.movie_id;
+  let qs = {
+    params: {
+      i: imdbId,
+      apikey: API_KEY
+    }
+  }
+  axios.get('http://www.omdbapi.com', qs )
+  .then(response => {
+    let movieData =  response.data;
+      res.render('detail', { data: movieData });
+  })
+  .catch(error => {
+        console.log('Error', error);
+  });
 });
 
 // The app.listen function returns a server handle
