6804b71ca0ee0e542aa268d6b079c87c99a2ba63
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-08-06 20:48:45 -0600

    [homework]mvp

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
+
+let API_KEY = process.env.API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,11 +20,51 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
 });
 
+app.get('/results', (req, res) => {
+  let movie = req.query.movie
+  let qs = {
+      params: {
+          s: movie,
+          apikey: API_KEY
+      }
+  }
+
+  axios.get('http://www.omdbapi.com', qs)
+  .then((response) => {
+      let matches = response.data.Search
+      res.render('results', {movies: matches})
+      // console.log(matches)
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  let imdbID = req.params.movie_id
+  let qs = {
+      params: {
+          i: imdbID,
+          apikey: API_KEY
+      }
+  }
+
+  axios.get('http://www.omdbapi.com', qs)
+  .then((response) => {
+    let movieData = response.data
+    res.render('detail', {data: movieData})
+    // console.log(movieData)
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+let server = app.listen(process.env.PORT || 3000);
 
 // We can export this server to other servers like this
 module.exports = server;
