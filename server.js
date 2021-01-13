1cfb8d7510ffbd76128394ef10924cc27b911600
Kenny Jean <kennyjean@Kennys-Mac-mini.local>
2021-01-13 00:49:26 -0500

    movie var renderd

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
+const { default: axios } = require('axios');
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const API_KEY = process.env.API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,12 +18,34 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
+//home route
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index.ejs')
 });
+
+//get route
+app.get('/results', (req,res) =>{
+  console.log(req.query)
+  axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${req.query.results}`)
+  .then(response => {
+    console.log(response.data)
+    res.render('results.ejs' , {movies:response.data.Search})
+  })
+})
+
+//show route
+app.get('/movies/:movieid', (req, res)=>{
+  console.log(req.params.movieid)
+  axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${req.params.movieid}`)
+  .then(response => {
+    console.log(response)
+    res.render('detail.ejs', {movie:response.data})
+  })
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
+
 
 // We can export this server to other servers like this
 module.exports = server;
