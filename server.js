2ee8b71eb08916c5a9dfa56ff5e3e169a17e105e
sameghosts <seanfeiner.brown@gmail.com>
2021-01-13 08:32:43 -0800

    got the second axios call as json now build out the details ejs page

--- server.js
+++ server.js
@@ -44,10 +44,17 @@ app.get('/results', (req, res) =>{
 //similar to how we refactored the prehistoric creatures and dinos will use a get route to display on the details linking from each movie using imdbID in the query results
 //stub it out first
 app.get('/movies/:movie_id', (req, res) => {
-  res.send('something');
-  // res.render()
-});
+  // res.send('something');
+  // console.log(req.body)
+  console.log(req.params.movie_id);
+  //new axios call for information
+  AXIOS.get(`http://www.omdbapi.com/?i=${req.params.movie_id}&apikey=${process.env.OMDB_API_KEY}`)
+  .then(response => {
 
+    res.send(response.data);
+  // res.render()
+  });
+});
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
 
