a1e676a974fd2ec533511ecc2f169354835b32ee
Jackson Franks <jacksonfranks@Jacksons-Air.lan>
2021-01-13 18:41:20 -0600

    Part 1 done

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios');
+const { response } = require('express');
+const API_KEY = process.env.API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -12,16 +15,33 @@ app.use(express.urlencoded({ extended: false }));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
 
-// Adds some logging to each request
-app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs')
 
-// Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+app.get('/', (req, res)=>{
+  res.render('index')
+})
 
-// The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+app.get('/results', (req, res) => {
+  console.log(req.query.searchTerm)
+  axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${req.query.searchTerm}`)
+  .then(response => {
+    let movieData = response.data.Search
+    console.log(response)
+      res.render('results.ejs', {myMovies: movieData})
+  })
+})
 
-// We can export this server to other servers like this
-module.exports = server;
+app.get('/results/:id', (req, res) => {
+  axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${req.params.id}`)
+  .then(response => {
+    console.log(response.data)
+    res.render('show', {movie: response.data})
+    // let movie = response.data.IMDBid
+    // res.render('/detail', {movieStuff: movie})
+  })
+})
+
+app.listen(3000, () => {
+  console.log('on port 3000')
+})
+
