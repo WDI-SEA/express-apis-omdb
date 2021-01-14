703a63ac6ab555845a91203e109e2c4af30859ae
Blake DeGraw <bbdegraw@gmail.com>
2021-01-13 20:44:14 -0800

    all done. it works

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ const API_KEY = process.env.API_KEY
 //vv to use ejs (after npm i ejs) then create a views folder with index.ejs inside)
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 //vv paired with line 4 from index.ejs
+
 app.get('/results', (req, res) => {
     console.log(req.query.searchTerm)
     axios.get('http://www.omdbapi.com/?apikey=' + API_KEY + '&s=' + req.query.searchTerm)
@@ -30,6 +32,24 @@ app.get('/movies/:movie_id', (req, res)=>{
         res.render('detail', {movie: response.data})
     }) 
     
+})
+
+// post to faves
+
+app.post('/faves', (req, res)=>{
+    db.fave.create({
+        title: req.body.title,
+        imdbid: req.body.imdbid
+    }).then(createdFav=>{
+        console.log(createdFav)
+        res.redirect('/faves')
+    })
+})
+
+app.get('/faves', (req, res)=>{
+    db.fave.findAll().then(faves=>{
+        res.render('faves.ejs', {faves: faves})
+    })
 })
 
 app.listen(3000)
