755c9f61e57eb006a5a55b01b195e766ed3b84ae
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2021-01-13 18:59:06 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
+const axios = require('axios');
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const API_KEY = process.env.API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,12 +18,28 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-// The app.listen function returns a server handle
+app.get('/results', (req, res) => {
+  console.log(req.query.searchTerm)
+  axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${req.query.searchTerm}`)
+  .then(response => {
+    console.log(response.data)
+      res.render('results', {movie: response.data.Search})
+  })
+})
+
+app.get('/results/:id', (req, res) => {
+  axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=n${req.params.id}`)
+  .then(response => {
+    res.send('show', {movie: response.data})
+  })
+})
+
+// // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
 
-// We can export this server to other servers like this
+// // We can export this server to other servers like this
 module.exports = server;
