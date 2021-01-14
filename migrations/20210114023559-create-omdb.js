29f65343148af71d36a7003bd4653f5da0e714d6
austintip <austin.d.tipograph@gmail.com>
2021-01-13 22:36:27 -0500

    faves saving

---
+++ migrations/20210114023559-create-omdb.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('omdbs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      imdb: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('omdbs');
+  }
+};
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const axios = require('axios');
+const methodOverride = require('method-override');
+let db = require('./models')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -10,6 +12,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('static'));
 // Sets up body-parser for parsing form data
 app.use(express.urlencoded({ extended: false }));
+// to PUT and DELETE routes
+app.use(methodOverride('_method'));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
 
@@ -42,6 +46,26 @@ app.get('/movies/:movie_id', function(req, res) {
       console.log(response.data);
       res.render('show', {movie: response.data});
     })
+})
+
+app.get('/faves', (req, res) => {
+  db.faves.findAll()
+  .then(movies => {
+    res.render('faves', {movies})
+  })
+})
+
+app.post('/faves', (req, res) => {
+  db.faves.create({
+    title: req.body.title,
+    imdbid: req.body.imdbID
+  })
+  .then(newMovie => {
+    console.log('New Movie here')
+    res.redirect('/faves')
+  }).catch(err => {
+    console.log('-----ERROR-----', err)
+  })
 })
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
