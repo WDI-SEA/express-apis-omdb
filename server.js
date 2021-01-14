ef92a0fc146f27c4a8adadd599116ae2e1ce5ab3
huiylu <74116599+huiylu@users.noreply.github.com>
2021-01-14 02:29:34 -0800

    Added POST and GET

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const axios = require('axios');
+const db = require('./models');
 let key=process.env.OMDB_API_KEY;
 
 // Sets EJS as the view engine
@@ -31,8 +32,39 @@ app.get('/results',(req,res)=>{
         })
   
 });
+/*
+app.get('/movies', (req,res)=>{
+    res.send('Test');
+});
+*/
+app.get('/results/:id',(req,res)=>{
+  axios.get(`http://www.omdbapi.com/?apikey=${key}&i=${req.params.id}`)
+        .then((response)=>{
+          console.log(req.params.id)
+          res.render('detail', {movie: response.data});
+        })
+});
 
-// The app.listen function returns a server handle
+app.get('/faves', (req,res)=>{
+    db.fave.findAll().then(faves=>{
+      console.log(faves);
+      res.render('faves',{movies: faves});
+    }).catch(err=>{
+      console.log(err);
+    });
+});
+
+app.post('/faves',(req,res)=>{
+  db.fave.create({
+    title: req.body.title,
+    imdbid: req.body.imdbid
+  }).then(fave=>{
+    res.redirect('/faves');
+  }).catch(err=> {
+    console.log(err);
+    res.redirect('/faves');   
+  }) 
+});
+
 app.listen(8000);
-
 // We can export this server to other servers like this
