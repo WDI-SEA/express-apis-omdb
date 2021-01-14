4e4c76162d4c138571dbbbf86de012c409249ddb
Drake Tyksinski <drake.tyksinski@live.com>
2021-01-13 20:45:58 -0800

    integrate sequelize and update routes and views

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const API_KEY = process.env.API_KEY
 // const url = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`
 const axios = require('axios')
+const db = require('./models')
 
 
 // Sets EJS as the view engine
@@ -29,7 +30,7 @@ app.get('/results', (req, res) => {
   axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${req.query.searchTerm}`)
     .then(response => {
       console.log(response.data.Search)
-      res.render('results', {movieResults: response.data.Search})
+      res.render('results', {movieResults: response.data.Search || []})
     })
 })
 
@@ -37,6 +38,27 @@ app.get('/movies/:movie_id', (req, res) => {
   axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${req.params.movie_id}`)
   .then(response => {
     res.render('detail', {movieDetail: response.data})
+  })
+})
+
+app.get('/faves', (req, res) => {
+  db.fave.findAll()
+  .then(favesFromDb => {
+    res.render('faves', {faveMovies: favesFromDb})
+  })
+})
+    
+
+app.post('/faves', (req, res) => {
+  console.log(req.body.title)
+  console.log(req.body.imdbid)
+  db.fave.create({
+    title: req.body.title,
+    imdbid: req.body.imdbid
+  })
+  .then(faveMovie=>{
+    console.log(faveMovie)
+    res.redirect('faves')
   })
 })
 
