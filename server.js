ebd0a7765d52630d640229cb48037d7ce76434e9
miknayr <teamry4n@gmail.com>
2021-06-02 19:34:56 -0700

    solve?

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
+const omdbApiKey = process.env.OMDB_API_KEY
+
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,12 +19,44 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+
+app.get('/results', (req, res) => {
+  console.log(`http://www.omdbapi.com/?apikey=${omdbApiKey}&s=${req.query.q}`);
+  axios.get(`http://www.omdbapi.com/?apikey=${omdbApiKey}&s=${req.query.q}`)
+      .then((response) => {
+        console.log('*** movies: ', response);
+        res.render('results', {movies: response.data.Search});
+      })
+      .catch(err => {console.log(err)})
+});
+
+
+app.get('/movies/:id', (req, res) => {
+  axios.get(`http://www.omdbapi.com/?apikey=${omdbApiKey}&i=${req.params.id}`)
+      .then((response) => {
+        console.log(req.params.id)
+        res.render('detail', {details: response.data});
+      })
+      .catch(err => {console.log(err)})
 });
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
 
+
 // We can export this server to other servers like this
 module.exports = server;
+
+
+
+
+// `~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// two ejs files
+// 1 
