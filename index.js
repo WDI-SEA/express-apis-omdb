f194788d44c57f298fbb2dfb920eb6f3004d80a9
Brian Young <berkshirehunt@gmail.com>
2021-06-03 10:36:46 -0700

    next am  touches

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
+// Import and config dotenv library
+require('dotenv').config()
 // import express
 const express = require('express')
 // import axios
 const axios = require('axios')
-// Import and config dotenv library
-require('dotenv').config()
 // declare instance of app
 const app = express()
 // open up a port for app to listen on
 const PORT = 3000
-
 // Define API key var
 const ombdApiKey = process.env.OMDB_API_KEY
-
 const fs = require('fs')
 // Express ejs layouts
 const layouts = require('express-ejs-layouts')
@@ -20,7 +18,7 @@ app.set('view engine', 'ejs')
 // Site starts at /public ->
 app.use(express.static('static'))
 app.use(layouts)
-
+///////////////////////////////////////////////
 // create home route
 app.get('/', (req, res) => {
   res.render('index')
