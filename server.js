8212d2acc94b24089aa9d21a13792afed6281a16
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-06-03 01:26:26 -0400

    it functions

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const { response } = require('express');
 const ejsLayouts = require('express-ejs-layouts')
 
-const PORT = 3000
+const PORT = 3001
 const omdbApiKey = process.env.OMDB_API_KEY 
 app.set('view engine', 'ejs')
 app.use(express.static('static'))
@@ -15,30 +14,29 @@ app.use(ejsLayouts)
 app.use(require('morgan')('dev'));
 
 //create a home route
+//create a home route
 app.get('/', (req, res) => {
-          res.render('index.ejs')
-      })
-
-
-//GET (read) movie results from input form
+  res.render('index.ejs')
+})
+// GET (read) movie results from input form
 app.get('/results', (req, res) => {
   let newObject = {
-      params: {
-          s: req.query.search,
-          apikey: omdbApiKey
-      }
+    params: {
+      s: req.query.search,
+      apikey: omdbApiKey
+    }
   }
   axios.get('http://www.omdbapi.com/', newObject)
-      .then(resFromApi => {
-          let results = resFromApi.data.Search
-          res.render('results', { results: results })
+  .then(resFromApi => {
+    let results = resFromApi.data.Search
+    res.render('results', { results: results })
+    let imdbRes = resFromApi.data.imdbID
+    res.render('results', { movies: imdbID})
+    })
+  .catch(err => {console.log(err)})
+  })
 
-          let imdbRes = resFromApi.data.imdbID
-          res.render('results', { movies: imdbID})
-      })
-      .catch(err => {console.log(err)})
-})
-
+//details
 app.get('/detail/:imdbID', (req, res) => {
   let qs = {
     params: {
@@ -47,18 +45,18 @@ app.get('/detail/:imdbID', (req, res) => {
     },
   }
   axios
-  .get('http://www.omdbapi.com', qs)
-  .then((resFromApi) => {
-    let movieData = resFromApi.data
-    console.log(movieData)
-    // res.render('detail', { q: movieData })
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+    .get('http://www.omdbapi.com', qs)
+    .then((resFromApi) => {
+      let movieData = resFromApi.data
+      console.log(movieData)
+      res.render('detail', { q: movieData })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })
 
-// POST movie 
+
 //open up a port for the app to listen on + define port
 app.listen(PORT, () => {
   console.log("youre listening to the smooth sounds of a movie app🎞")
