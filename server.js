5ea3989719a5c277acafc83e342f74cd28964c92
hamazumi <khamamoto12@gmail.com>
2021-06-03 11:00:03 -0700

    added Anna's solve

--- server.js
+++ server.js
@@ -13,21 +13,23 @@ const ejsLayouts = require('express-ejs-layouts')
 //Define API key var retreived from .env file
 const omdbApiKey = process.env.OMDB_API_KEY
 
-// Sets EJS as the view engine
+// Sets EJS as the view engine (Ability to use view folder and names)
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
 app.use(express.static('static'));
-// Sets up body-parser for parsing form data
+// Sets up body-parser for parsing form data (Ability to use <form>)
 app.use(express.urlencoded({ extended: false }));
-// Enables EJS Layouts middleware
+// Enables EJS Layouts middleware (ability to use ejs <%-Body>)
 app.use(ejsLayouts);
 
 // Routes
+//Create home route
 app.get('/', (req,res) => {
+  //Use of ejs and ejs layouts below. You can just put index instead of index.ejs
   res.render('index')
 })
 
-//Create home route
+//Create /results route
 app.get('/results', (req, res) => {
   let newObject = {
     params:{
@@ -45,6 +47,34 @@ app.get('/results', (req, res) => {
       .catch(err => {console.log(err)})
 })
 
+
+
+// Anna's solve
+// Create /results route
+// app.get('/results', (req, res) => {
+//   // .search below is from the "name="search" from index.ejs form
+//   let movieTitle = req.query.search
+  
+//   //ORIGINAL REQUEST for information, start with /? to start query &apikey
+//   axios.get(`http://www.omdbapi.com/?t=${movieTitle}&apikey=${process.env.OMDB_API_KEY}`)
+//   //RESPONSE FROM API (ACCESS to informaiton)
+//   .then((resFromAPI) => {
+//     //console.log to see what data you're getting
+//     console.log(resFromAPI.data)
+//     let title = resFromAPI.data.title
+//     let year = resFromAPI.data.Year
+//     let plot = resFromAPI.data.Plot
+//     let imdbID = resFromAPI.data.imdbID
+//         //renders data into results.ejs
+//         res.render('results', {title: title, year: year, plot: plot, imdbID: imdbID})
+//       })
+//       .catch(err => {console.log(err)})
+// })
+
+
+
+
+//Create /detail route
 app.get('/detail/:imdbID', (req, res) => {
   let newObject = {
     params: {
@@ -56,8 +86,9 @@ app.get('/detail/:imdbID', (req, res) => {
   .then((resFromAPI) => {
     console.log(resFromAPI.data.Plot)
     let details = resFromAPI.data
-    res.render('detail', {detailsX: details})
+    res.render('detail', {details: details})
   })
+  .catch(err => {console.log(err)})
 })
 
 //open up a port for app to listen
