b3bf12ef7ab8ceb737abcf4b171afaf436757fd9
funbunch <shannan_bunch@hotmail.com>
2021-06-02 21:39:57 -0700

    Added working solution to display detail page. not styled yet

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
+require('dotenv').config();
+const PORT = 4010
+//define API key var
+const omdbApiKey = process.env.OMDB_API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,12 +20,84 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/results', (req, res) => {
+  //get to external location using axios
+  let qs = {
+    params: {
+      s:req.query.q,
+      apikey:omdbApiKey
+    }
+  }
+  axios.get('http://www.omdbapi.com', qs)
+    .then((response) => {
+      let results = response.data.Search
+      console.log(results)
+      res.render('results', {movies:results})
+    }) 
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+app.get('/movies/:id', (req, res) => {
+  //get to external location using axios
+  const id = req.params.id
+  let qs = {
+    params: {
+      id:req.params.id,
+      apikey:omdbApiKey
+    }
+  }
+  axios.get('http://www.omdbapi.com/', qs)
+    .then((result) => {
+      console.log(result)
+      res.render('detail')
+    }) 
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+// app.get('/detail/:imdbID', (req, res) => {
+//     let qs =  {
+//       params: {
+//         i: req.params.imdbID,
+//         apikey:omdbApiKey,
+//       }
+//     }
+//     axios.get('http://www.omdbapi.com/',qs)
+//     .then((result) => {
+//       let movieData = result.data
+//       res.render('detail', {q:movieData })
+//     })
+//     .catch(err) => {
+//       console.log(err)
+// })
+app.get('/detail/:imdbID', (req, res) => {
+  let qs =  {
+     params: {
+      i: req.params.imdbID,
+     apikey:omdbApiKey,
+    }
+  }
+  axios.get('http://www.omdbapi.com/',qs)
+  .then((result) => {
+    let movieData = result.data
+    res.render('detail', {q:movieData })
+  })
+  .catch(err => {
+    console.log(err)
+})
+})
+
+
+
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
-
-// We can export this server to other servers like this
-module.exports = server;
+app.listen(PORT, () => {
+  console.log('welcome to our very smooth port')
+})
