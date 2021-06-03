0eae7042877f4fa0ba900a17415ab3ba3f9961ad
BeckyBoo88 <rbooco07@gmail.com>
2021-06-02 18:21:41 -0600

    figuring out routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
+const omdbApiKey = process.env.OMDB_API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -12,16 +14,33 @@ app.use(express.urlencoded({ extended: false }));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
 
-// Adds some logging to each request
-app.use(require('morgan')('dev'));
-
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+// Home page with form to ask about specific movie
+app.get('/', (req, res) => {
+  axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${omdbApiKey}`)
+      .then((resFromAPI) => {
+          console.log(resFromAPI.data.Title)
+          res.send(resFromAPI.data.Title)
+      })
+      .catch(err => {console.log(err)})
+})
+// GET /movieResults
+app.get('/results', (req, res) => {
+  axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${omdbApiKey}`)
+      .then((resFromAPI) => {
+          console.log(resFromAPI.data)
+          res.send(resFromAPI.data)
+      })
+      .catch(err => {console.log(err)})
+})
+
+// GET /moveResults/:id -- READ specific movie details
+
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+app.listen(4000, () => {
+  console.log("Can you hear me now???")
+});
 
-// We can export this server to other servers like this
-module.exports = server;
+
+
