5252136769226609f0676cfd899a6c40b3177072
erikwinkel <erikpwinkel@gmail.com>
2021-06-02 20:20:39 -0700

    solution

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
-require('dotenv').config();
-const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const axios = require('axios')
+const app = express()
+require('dotenv').config()
+
 
 // Sets EJS as the view engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 // Specifies the location of the static assets folder
-app.use(express.static('static'));
+app.use(express.static('static'))
 // Sets up body-parser for parsing form data
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 // Enables EJS Layouts middleware
-app.use(ejsLayouts);
+app.use(ejsLayouts)
 
-// Adds some logging to each request
-app.use(require('morgan')('dev'));
+const omdbApiKey = process.env.OMDB_API_KEY
 
 // Routes
+
+// Home route
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+  res.render('index')
+})
+
+// results route - displays search results
+app.get('/results', (req,res) => {
+  axios.get(`http://www.omdbapi.com/?s=${req.query.q}&type=movie&apikey=${omdbApiKey}`)
+    .then((response) => {
+      let searchResults = response.data.Search
+      res.render('results', { searchResults: searchResults, search: req.query.q })
+    })
+    .catch(err => console.log(err))
+})
+
+// movies/:movie_id route - shows details of the movie
+app.get('/movies/:movie_id' , (req,res) => {
+  axios.get(`http://www.omdbapi.com/?i=${req.params.movie_id}&apikey=${omdbApiKey}`)
+    .then((response) => {
+      res.render('detail', {details: response.data, keys: Object.keys(response.data)})
+    })
+    .catch(err => console.log(err))
+})
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000)
 
 // We can export this server to other servers like this
-module.exports = server;
+module.exports = server
