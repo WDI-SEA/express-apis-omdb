82bd8a1ef0905871d60536d39bed79c027738dbc
cameronjoy <camlangdev@gmail.com>
2021-06-08 13:42:16 -0400

    solve

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios')
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
@@ -17,11 +18,43 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
 });
 
-// The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+app.get('/results', function(req, res) {
+  // store the value of the param string from req.query
+  let movieTitle = req.query.q
 
-// We can export this server to other servers like this
-module.exports = server;
+  // use axios to make a call to OMDB API with query string
+  axios.get(`http://www.omdbapi.com/?t=${movieTitle}&apikey=${process.env.OMDB_API_KEY}`)
+    .then(APIres => {
+      let title = APIres.data.Title
+      let year = APIres.data.Year
+      let plot = APIres.data.Plot
+      let imdbID = APIres.data.imdbID
+      
+      console.log(title, year, plot, imdbID)
+      // res.render results to results.ejs
+      res.render('results', {title: title, year: year, plot: plot, imdbID: imdbID})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
+
+    app.get('/movies/:movie_id', function(req, res) {
+      // store movie_id to a variable
+      let imdbID = req.params.movie_id
+    
+      // make an axios call to the OMDB API for full data obj for movie_id
+      axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${imdbID}`)
+        .then(APIres => {
+          res.send(APIres.data)
+        })
+        .catch(err => console.log(err))
+    })
+
+
+app.listen(process.env.PORT || 3000, () => {	
+  console.log(`Listening to the sweet sweet sounds of ${process.env.PORT} in the morning ☕️.`)
+})
