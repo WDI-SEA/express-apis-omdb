1c4c2151d838d36dfaf4b37622dc93d2a4873187
Norman Teodoro <normanteodoro@hotmail.com>
2022-02-19 21:34:04 -0800

    add error handling on invalid searches

--- server.js
+++ server.js
@@ -1,53 +1,60 @@
-require('dotenv').config();
-const Router= require('express');
-const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
-const app = express();
+require('dotenv').config()
+const Router = require('express')
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const app = express()
 const axios = require('axios')
 
 // Sets EJS as the view engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // Specifies the location of the static assets folder
-app.use(express.static('static'));
+app.use(express.static('static'))
 
 // Sets up body-parser for parsing form data
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
 // Enables EJS Layouts middleware
-app.use(ejsLayouts);
+app.use(ejsLayouts)
 
 // Adds some logging to each request
-app.use(require('morgan')('dev'));
+app.use(require('morgan')('dev'))
 
 // Routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index')
-});
-app.get('/results', function(req, res) {  
-  if(req.query.search=="") {
+})
+app.get('/results', function (req, res) {
+  console.log("🚀 ~ file: server.js ~ line 28 ~  res",  res)
+  if (req.query.search == '') {
     res.render('error')
   } else {
     const url = `https://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${req.query.search}`
-   axios.get(url)
-   .then(response => {
-      res.render('results',{searchResults : response.data.Search,
-      searchQuery : req.query.search})
-   })
-   .catch((err)=> {
-     if (err) res.render('error')
-   })   
+    axios.get(url)
+      .then(response => {
+        if(response.data.Search == null) {
+          res.render('error')
+        } else {
+          res.render('results', {
+          searchResults: response.data.Search,
+          searchQuery: req.query.search
+          })
+        } 
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
-  
-  });
+})
 
-app.get('/detail', (req,res)=> {
+app.get('/detail', (req, res) => {
+  
   let queryString = req.query.i
   const urlDetails = `https://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${queryString}`
   axios.get(urlDetails)
-   .then(r =>{
-    console.log(r.data)
-    res.render('detail.ejs', {
+  .then(r => {
+    
+      res.render('detail.ejs', {
       Title: r.data.Title,
       Year: r.data.Year,
       Rated: r.data.Rated,
@@ -73,16 +80,16 @@ app.get('/detail', (req,res)=> {
       Production: r.data.Production,
       Website: r.data.Website,
       Response: r.data.Response
-    })  
+    })
   })
-  // .catch(err)  
+  // .catch(err)
 })
 
 // The app.listen function returns a server handle
-app.listen(process.env.PORT,err =>{
-  if(err) console.log(err)
+app.listen(process.env.PORT, err => {
+  if (err) console.log(err)
   console.log(`Server listening to port ${process.env.PORT} 🎧`)
-});
+})
 
 // We can export this server to other servers like this
 module.exports = Router
