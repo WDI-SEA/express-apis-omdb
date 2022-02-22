b8c5172427c44af0731f44c023b423fb0a75c9bf
taekim90 <taehk15@gmail.com>
2022-02-22 09:03:27 -0800

    completion of deliverable

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
+
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,8 +19,33 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index.ejs');
 });
+
+app.get('/results', (req, res) => {
+  // console.log(req.query.search)
+  // console.log(req)
+  const url = `http://www.omdbapi.com/?t=${req.query.q}&apikey=${process.env.OMDB_API_KEY}`
+  axios.get(url)
+    .then(response => {
+      // console.log(response)
+      const searchResults = response.data
+      // console.log(searchResults)
+      res.render('results.ejs', { response: searchResults })
+    })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  // console.log(req.params.movie_id)
+  // res.send("Id is " + req.params.movie_id)
+  const url = `https://www.omdbapi.com/?i=${req.params.movie_id}&apikey=${process.env.OMDB_API_KEY}`
+  axios.get(url)
+    .then(response => {
+      // res.send(response.data)
+      const searchResults = response.data
+      res.render('detail.ejs', { response: searchResults })
+    })
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
