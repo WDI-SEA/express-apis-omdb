dbbd5837c0dd32a297f2ac59d323510b1ff00a33
Jon Dimaculangan <jondmclngn@gmail.com>
2022-02-22 01:04:04 -0600

    error handling and back btn added

--- controllers/search.js
+++ controllers/search.js
@@ -4,16 +4,19 @@ const axios = require('axios')
 // const { response } = require('express')
 require('dotenv').config()
 
+let search
 
 router.get('/', (req, res) => {
     const url = `http://www.omdbapi.com/?s=${req.query.search}&apikey=${process.env.OMDB_API_KEY}`
     axios.get(url)
         .then(response => {
             const searchResults = response.data.Search
+            search = req.query.search
             // id = response.data.Search.imdbID
-            res.render('results.ejs', {results:searchResults})
+            res.render('results.ejs', { results: searchResults })
             // res.send(searchResults)
         })
+        // .catch(console.log(err))
 })
 
 router.get('/movies/:movie_id', (req, res) => {
@@ -22,7 +25,7 @@ router.get('/movies/:movie_id', (req, res) => {
         .then(response => {
             const searchResults = response.data
             const ratings = response.data.Ratings
-            res.render('detail.ejs', {results:searchResults, rates:ratings})
+            res.render('detail.ejs', {results:searchResults, rates:ratings, title:search})
             // res.send(searchResults)
             // res.render('detail.ejs', {results:req.params.movieId})
         })
