50a290e6e7da4e28464af8eee7d9d6cbc15503af
“ikarabag1” <irmakkarabag@gmail.com>
2022-02-24 04:23:28 +0300

    part 2 completed

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
+const db = require('./models')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -11,17 +13,60 @@ app.use(express.static('static'));
 app.use(express.urlencoded({ extended: false }));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
-
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
 
-// Routes
+// ROUTES
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index.ejs');
 });
+
+// ASYNC ROUTE lets you use async/await in an express route
+app.get('/results', async(req, res) => {
+    try {
+      // define a var that whatever bring back from response
+      const response = await axios.get(`http://www.omdbapi.com/?s=${req.query.movieSearch}&apikey=${process.env.OMDB_API_KEY}`)
+      res.render('results.ejs', { movies: response.data.Search })
+    } catch (error) {
+      console.log(error)
+    }
+})
+
+app.get('/details/:id', (req, res) => {
+  console.log(req.params.id)
+  axios.get(`http://www.omdbapi.com/?i=${req.params.id}&apikey=${process.env.OMDB_API_KEY}`)
+    .then(response => {
+      res.render('detail.ejs', { movie: response.data })
+    })
+    .catch(console.log)
+})
+
+// GET ROUTE/FAVES -- READ all faves from the db
+app.get('/faves', async (req, res) => {
+  try {
+      const allFaves = await db.fave.findAll()
+      res.json(allFaves)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+// POST ROUTE/FAVES -- CREATE a fave and redirect to /faves -- redirect to another route -- res.send sends a string
+app.post('/faves', async (req, res) => {
+  try {
+    await db.fave.create({
+      title: req.body.title,
+      imdbId: req.body.imdbId
+    })
+// perform get request
+    res.redirect('/faves')
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
 
-// We can export this server to other servers like this
-module.exports = server;
