e48a33e28766d1641e45bd19b5a809611e202e6a
Billy Lu <billyfwlu@gmail.com>
2022-05-23 09:19:37 -0700

    Finished deliverable with .env implementation

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const axios = require('axios');
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
@@ -16,9 +17,39 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+app.get("/",function(req,res){
+  res.render("index.ejs")
+  // console.log(req)
+})
+
+app.get('/results', async function(req, res) {
+  // res.send('Hello, backend!');
+  try {
+
+    // console.log(req.query.userInput)
+    const searchURL = `http://www.omdbapi.com/?s=${req.query.userInput}&apikey=${process.env.API_KEY}` 
+    const response = await axios.get(searchURL)
+      console.log(response.data.Search)
+    res.render("results.ejs",
+     { movies: response.data.Search })
+  } catch(err) {
+    console.warn(err)
+  }
 });
+
+
+app.get("/results/detail", async function(req,res){
+  try {
+    console.log(req.query.id)
+    const idURL = `http://www.omdbapi.com/?i=${req.query.id}&apikey=${process.env.API_KEY}` 
+    const response = await axios.get(idURL)
+    // console.log(response.data)
+    res.render("detail.ejs", {details: response.data})
+
+  } catch(err) {
+    console.warn(err)
+  }
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
