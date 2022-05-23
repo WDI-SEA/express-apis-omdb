0f2bc4837e22db5d2afcc397fb07bc096fef86cc
mitchdmarino <mitchdmarino@gmail.com>
2022-05-23 09:21:54 -0700

    finished project hidden api key

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+//dot env 
 require('dotenv').config();
+
+
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,11 +21,44 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
 });
+app.get('/results', function(req,res) {
+  const searchURL = `https://www.omdbapi.com/?s=${req.query.q}&apikey=${process.env.API_KEY}`
+  axios.get(searchURL)
+  .then(response => {
+    
+    console.log(response.data.Search)
+    res.render('results', {
+      query: req.query.q, 
+      movies: response.data.Search
+      
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.send('Error. No movies found.')
+  })
+  
+})
+
+app.get('/movies/:movie_id', (req,res) => {
+  console.log(req.params, 'query')
+  const detailsURL = `https://www.omdbapi.com/?i=${req.params.movie_id}&apikey=${process.env.API_KEY}`
+  axios.get(detailsURL) 
+  .then(response => {
+    console.log(response.data)
+    res.render('detail', {
+      movie : response.data
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3001);
 
 // We can export this server to other servers like this
 module.exports = server;
