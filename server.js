2d1b8879e4cd0eafc64b2e70509373a788a9f74b
GianlorenzoDercole <johndercole01@gmail.com>
2022-05-23 09:20:24 -0700

    complete

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
+const axios = require('axios')
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
@@ -16,10 +17,42 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello, backend!');
+// });
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
 
+app.get('/search', async (req, res) => {
+  try {
+      const searchUrl = `http://www.omdbapi.com/?t=${req.query.userInput}&apikey=${process.env.API_KEY}`
+      const response = await axios.get(searchUrl)
+      console.log(searchUrl)
+      console.log(response)
+      res.render('results.ejs', {
+        title: response.data.Title,
+        rating: response.data.Rated,
+        year: response.data.Year
+
+        })
+  } catch (err) {
+      console.log('t')
+  }
+})
+// app.get('/search/details', (req, res) => {
+//   axios.get(req.query.url)
+//       .then(response =>  {
+//           console.log(response.data)
+//           res.render('details.ejs', {title: response.data.Title})
+//       })
+
+// })
+
+// axios.get('http://www.omdbapi.com/?apikey=[f22f8b87]&/t=rocky')
+//   .then(function (response) {
+//     console.log(response);
+//   })
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
 
