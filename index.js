34c3318dfa903fbe7b543d546edf6c208a845faf
syorn96 <syorn2014@gmail.com>
2022-09-03 21:32:27 -0700

    attempts to write correct link for individual movies based on imbd id

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 // Install and require packages
 const express = require('express')
 const axios = require('axios')
-
+const ejsLayouts = require('express-ejs-layouts');
 // config instance of express
 const app = express()
 const PORT = 8080
 
 // set the view engine
 app.set('view engine', 'ejs')
-
+app.use(ejsLayouts);
 // define some routes
+const apiKey = `&apikey=e40f21b5`
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
 app.get('/results', (req,res) => {
-    console.log(req.query)
-    const apiKey = `&apikey=e40f21b5`
-    const url = `http://www.omdbapi.com/?t=${req.query.userInput}${apiKey}`
+    // console.log(req.query)
+    const url = `http://www.omdbapi.com/?s=${req.query.userInput}${apiKey}`
     axios.get(url)
         .then(response => {
             console.log(response.data)
             res.render('results.ejs', {
                 input: req.query.userInput,
-                movie: response.data
+                movie: response.data.Search,
             })
         })
         .catch(console.error)
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+    const url = `http://www.omdbapi.com/?i=${req.query}${apiKey}`
+    axios.get(url)
+    .then(response => {
+        console.log(response.data)
+        res.render('detail.ejs', {
+            movie: response.data,
+        })
+    })
+    .catch(console.error)
 })
 
 
