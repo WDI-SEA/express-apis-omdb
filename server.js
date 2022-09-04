90f7db5e50432a7aa895536d81101ec8c94fb07f
ADRIANRROJO <rojoadrian16@gmail.com>
2022-09-03 20:07:07 -0500

    need to solve forEach loop to display all titles

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
 
+const PORT = 8000;
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
@@ -17,11 +19,23 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index.ejs')
 });
 
+app.get("/results", (req, res) => {
+  let searchTerm = req.query.search;
+  let url = "https://www.omdbapi.com/?s=" + searchTerm + "&apikey=2fdc7ef0 ";
+  axios.get(url)
+  
+  .then(response => {
+      console.log(response.data.Search);
+  res.render("results.ejs", {data: response.data.Search});
+  })
+  .catch(err => {
+      console.log(err);
+  })}) 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 8000);
 
 // We can export this server to other servers like this
 module.exports = server;
