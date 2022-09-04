81154d8bca034c322e2e24cfddde00b919850acf
ADRIANRROJO <rojoadrian16@gmail.com>
2022-09-04 12:46:04 -0500

    finished deliverable

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const axios = require('axios')
-
 const PORT = 8000;
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -26,14 +25,27 @@ app.get("/results", (req, res) => {
   let searchTerm = req.query.search;
   let url = "https://www.omdbapi.com/?s=" + searchTerm + "&apikey=2fdc7ef0 ";
   axios.get(url)
+  // Sends the user to the results page
+  .then(response => {
+    res.render("results.ejs", 
+		{
+			data: response.data,
+		});
+	}).catch(console.warn);
+})
+// sends the user to the selected movie page
+app.get("/movies/:id", (req, res) => {
+  let movieId = req.params.id;
+  let url = "https://www.omdbapi.com/?i=" + movieId + "&apikey=2fdc7ef0 ";
+  axios.get(url)
   
   .then(response => {
-      console.log(response.data.Search);
-  res.render("results.ejs", {data: response.data.Search});
-  })
-  .catch(err => {
-      console.log(err);
-  })}) 
+    res.render("detail.ejs", 
+    {
+      data: response.data,
+    });
+  }).catch(console.warn);
+})
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 8000);
 
