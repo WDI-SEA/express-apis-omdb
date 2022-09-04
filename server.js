07e123b62c03340e412e998195504b50657b4eec
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-03 17:24:22 -0700

    add titles and links to IMDB to results.ejs

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios');
 const app = express();
-
+// const apiKey = '6efcn7db'
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
@@ -17,11 +18,26 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index.ejs');
 });
+app.get('/results', (req,res)=>{
+  // console.log(req.query)
+  const url = `https://www.omdbapi.com/?apikey=6efcb7db&s=${req.query.userInput}`
+  axios.get(url)
+    .then(response =>{
+      // console.log(url)
+      console.log(response.data.Search)
+      res.render('results.ejs', {
+        input: req.query.userInput,
+        movies: response.data.Search
+      })
+      
+    })
+    .catch(console.warn)
 
+})
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
-
+console.log('server up')
 // We can export this server to other servers like this
 module.exports = server;
