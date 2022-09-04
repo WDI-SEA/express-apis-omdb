2c3f2f00a1dc50af6593cd7731f55566e391dd79
iandab47 <dabinett.ian@gmail.com>
2022-09-04 12:03:40 -0500

    Part 1 completed with dotenv processing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios')
 const app = express();
 
 // Sets EJS as the view engine
@@ -17,11 +18,40 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index')
 });
 
+app.get('/search', (req, res) => {
+  const searchUrl = `http://www.omdbapi.com/?${process.env.API_KEY}&s=${req.query.s}`
+  const input = req.query.s
+  if(req.query.s === '') {
+    res.render('500')
+  }
+  axios.get(searchUrl)
+    .then(response => {
+      const search = response.data.Search
+      // console.log(searchUrl, input, search)
+      res.render('results', { search, input })
+    })
+    .catch(console.warn)
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const id = req.params.movie_id
+  const reqUrl = `http://www.omdbapi.com/?${process.env.API_KEY}&i=${id}`
+  axios.get(reqUrl)
+    .then(response => {
+      const movie = response.data
+      res.render('detail', { movie })
+    })
+    .catch(console.warn)
+})
+
+
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT);
 
 // We can export this server to other servers like this
 module.exports = server;
+
+// omdb data request: http://www.omdbapi.com/?apikey=ab979836&?s=
