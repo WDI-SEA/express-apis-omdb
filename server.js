7bab31b5474d8bf53b299a1c8afd1407b57c1652
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-06 09:28:32 -0700

    hidden key

--- server.js
+++ server.js
@@ -2,26 +2,56 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios');
+const { response } = require('express');
 
-// Sets EJS as the view engine
+
+const myKey = process.env.API_KEY
+
+
+
 app.set('view engine', 'ejs');
-// Specifies the location of the static assets folder
 app.use(express.static('static'));
-// Sets up body-parser for parsing form data
 app.use(express.urlencoded({ extended: false }));
-// Enables EJS Layouts middleware
 app.use(ejsLayouts);
-
-// Adds some logging to each request
 app.use(require('morgan')('dev'));
 
+
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-// The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+app.get('/results', (req,res) => {
+  
+  const url = `https://www.omdbapi.com/?t=${req.query.q}&apikey=${myKey}`
+  axios.get(url)
+    .then(response => {
+      console.log(response.data)
+      let dta = response.data
+      let infoPacket =  {
+        Title: dta.Title,
+        Year: dta.Year,
+        'Release Date': dta.Released,
+    }
+      res.render('results', {infoPacket, id : dta.imdbID, img : dta.Poster })
+    })
+    .catch(console.error)
+})
 
-// We can export this server to other servers like this
+app.get('/movies/:movie_id', (req,res) => {
+    const url = `https://www.omdbapi.com/?i=${req.params.movie_id}&apikey=${myKey}`
+    axios.get(url)
+      .then(response => {
+        console.log(response.data)
+        let dta = response.data
+        res.render('detail', {dta})
+      })
+      .catch(console.error)
+})
+
+
+let server = app.listen(process.env.PORT || 3000);
+
+
 module.exports = server;
