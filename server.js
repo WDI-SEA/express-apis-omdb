959f5581ffe4ff05fb22a1fe5425f1d79a0918c8
joshdewhurst <joshuardewhurst@gmail.com>
2022-09-06 09:25:43 -0700

    1st commit

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios');
+const { response } = require('express');
+const apiKey = process.env.API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,9 +19,33 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+app.get('/', (req, res) => {
+  res.render('index');
 });
+
+app.get('/search', (req, res) => {
+  const url = `http://www.omdbapi.com/?s=${req.query.userInput}${apiKey}`;
+  axios.get(url)
+  .then(response => {
+    res.render('results', {
+      movieResults: response.data,
+      movieInput: req.query.userInput
+    });
+    // console.log(response.data)
+  })
+  .catch(console.warn);
+})
+
+app.get('/movie/:movie_ID', (req, res) => {
+  const url = `http://www.omdbapi.com/?i=${req.params.movie_ID}&${apiKey}`;
+  axios.get(url)
+  .then (response => {
+    res.render('detail' , {
+      movie: response.data
+    })
+    console.log(response.data)
+  })
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
