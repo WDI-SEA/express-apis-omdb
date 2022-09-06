0440b7e67212e5215b1f7bfa7dc222c0a88175ff
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-06 09:29:45 -0700

    finsish deliverable

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const axios = require('axios');
 const app = express();
-// const apiKey = '6efcn7db'
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
@@ -16,6 +15,8 @@ app.use(ejsLayouts);
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
 
+console.log("process.env:", process.env)
+
 // Routes
 app.get('/', function(req, res) {
   res.render('index.ejs');
@@ -24,10 +25,10 @@ app.get('/', function(req, res) {
 //initial API call
 app.get('/results', (req,res)=>{
   // console.log(req.query)
-  const url = `https://www.omdbapi.com/?apikey=6efcb7db&s=${req.query.userInput}`
+  const url = `https://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${req.query.userInput}`
   axios.get(url)
     .then(response =>{
-      // console.log(url)
+      console.log(url)
       // console.log(response.data.Search)
       res.render('results.ejs', {
         input: req.query.userInput,
@@ -43,7 +44,7 @@ app.get('/results', (req,res)=>{
 app.get('/movies/:movie_id', (req,res)=>{
   console.log(req.query)
   const movie_id = req.params.movie_id
-  const url = `http://www.omdbapi.com/?apikey=6efcb7db&i=${movie_id}`
+  const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${movie_id}`
   console.log(url)
   axios.get(url)
     .then(response =>{
