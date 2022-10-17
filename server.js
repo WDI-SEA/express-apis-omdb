5b3602906f4a842da24fa3a8eafcdb0527e1d6cb
sumayajassim <sumayajassim1@gmail.com>
2022-10-18 01:03:03 +0300

    add the routers + display the data

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
-
+const axios = require('axios');
+const bodyParser = require("body-parser"); 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
 app.use(express.static('static'));
 // Sets up body-parser for parsing form data
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
 
@@ -17,8 +18,25 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  // res.send('Hello, backend!');
+  console.log(process.env.RANDOM_ENV_VAR);
+  res.render('index');
 });
+
+app.get('/results', (req,res)=>{
+  axios.get(`http://www.omdbapi.com/?&apikey=${process.env.API_KEY}&s=${req.query.s}`)
+  .then(function (response) {
+    res.render('results', {movies: response.data.Search})
+  })
+})
+
+app.get('/movies/:id', (req,res)=>{
+  axios.get(`http://www.omdbapi.com/?&apikey=${process.env.API_KEY}&i=${req.params.id }`)
+  .then(function(response){
+    res.render('detail', {movie: response.data})
+  })
+})
+
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
