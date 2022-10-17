75fcc7b51441c73727ad2062b2664cb5a78d28a4
Ameera Ali <amiraabdullamir@gmail.com>
2022-10-17 21:38:03 +0300

    funct: index,results

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
-
+//whatever process.env.PORT is defined use it 
 // Sets EJS as the view engine
+
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
 app.use(express.static('static'));
@@ -17,11 +20,30 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render("index")
+});
+app.get('/results', function(req, res) {
+  let actualRes = req.query.movName
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${actualRes}`)
+  .then(function (response) {
+    res.render("results",{resultTitle: response.data.Title, imdbID: response.data.imdbID})
+  })
+  app.get("/movies/:idx",(req,res)=>{
+    
+    axios.get(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${req.params.idx}}`)
+  .then(function (response) {
+    console.log(response)
+  
+    res.render("detail",{result:"hi"})
+  })
+  })
+
 });
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000,()=>{
+  console.log("LETS GOOOOO")
+});
 
 // We can export this server to other servers like this
 module.exports = server;
