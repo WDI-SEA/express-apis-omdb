28b4416d072f61a2e5a3dfe9081a38b1c02c63d0
Layla <layla.baqer97@gmail.com>
2022-10-18 18:19:02 +0300

    Part 1 done

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const request = require('request')
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+
+const axios = require('axios')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -19,29 +20,35 @@ app.use(require('morgan')('dev'));
 // Routes
 app.get('/', function(req, res) {
   console.log(process.env.OMDP_API_KEY)
-
-  request(`http://www.omdbapi.com/?apikey=[${process.env.OMDP_API_KEY}]&`, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log('request error')
-      res.send(body)
-    }
     res.render('index.ejs');
-  })
-
-});
-
-app.get('/results', function(req, res) {
-  // let movieFilter = req,query.movieFilter
-
-  res.render('results.ejs')
 })
 
+app.get('/results', (req, res)=>{
+  // let movieFilter = req,query.movieFilter
+  console.log(req.query)
 
+  // /?t gives one result, /?s gives a list of movies
+  axios.get(`http://www.omdbapi.com/?s=${req.query.q}&apikey=${process.env.OMDP_API_KEY}`)
+  .then(results=>{
+    console.log(results.data)
+    // res.render must be inside the .then to make sure it will run after we recieve the data
+    res.render('results.ejs', {results: results.data.Search})
+  })
+  .catch(err=>{
+    console.log("Error retrieving API: ", err)
+  })
 
+})
 
+app.get('/movies/:id', (req, res)=>{
+  console.log(req.params)
+  axios.get(`http://www.omdbapi.com/?i=${req.params.id}&apikey=${process.env.OMDP_API_KEY}`)
+  .then(movieDetails=>{
+    console.log(movieDetails.data)
+    res.render('detail.ejs', movieDetails.data)
+  })
 
-
-
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
