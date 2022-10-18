0b7e06e8bea7967a2ef27951d6f70beb4e9db838
Fuad AlShuroogi <fmjs@Fuads-MacBook-Air.local>
2022-10-18 19:45:34 +0300

    Fuad AlShuroogi - Done

---
+++ controllers/main.js
@@ -0,0 +1,31 @@
+const express = require('express'),
+router = express.Router(),
+axios = require('axios')
+
+router.get('/', (req,res) => {
+    res.render('index.ejs')
+})
+
+router.get('/results', (req, res) => {
+    console.log(req.query)
+
+    axios.get(`http://www.omdbapi.com/?s=${req.query.q}&apikey=${process.env.API_KEY}`)
+        .then((results) => {            
+                res.render('results', { results: results.data.Search })
+
+        })
+})
+
+router.get('/movies/:id', (req, res) => {
+    console.log(req.query)
+    axios.get(`http://www.omdbapi.com/?i=${req.params.id}&apikey=${process.env.API_KEY}`)
+        .then((movieDetails) => {
+            console.log(movieDetails)
+            res.render('detail', { movies: movieDetails.data })
+        })
+
+})
+
+// Routes
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 require('dotenv').config();
-const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
-const app = express();
+const express = require('express'),
+    ejsLayouts = require('express-ejs-layouts'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    axios = require('axios')
 
-// Sets EJS as the view engine
-app.set('view engine', 'ejs');
-// Specifies the location of the static assets folder
-app.use(express.static('static'));
-// Sets up body-parser for parsing form data
-app.use(express.urlencoded({ extended: false }));
-// Enables EJS Layouts middleware
-app.use(ejsLayouts);
 
-// Adds some logging to each request
-app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+app.use(express.static('public'));
+app.use('/', require('./controllers/main'))
 
-// Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
 
-// The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
-
-// We can export this server to other servers like this
-module.exports = server;
+app.listen(port);
