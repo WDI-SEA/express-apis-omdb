398fe26b84bb5cfb4c83152cff0e225d2cf749e9
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-10-18 21:36:18 +0300

    Part Completed

--- controllers/main.js
+++ controllers/main.js
@@ -1,17 +1,15 @@
+// Importing the required libraries
 const { render } = require('ejs')
 const express = require('express')
 const axios = require('axios');
+const db = require('../models')
+const fs = require('fs')
 
 const router = express.Router()
 
 
-const fs = require('fs')
-
-
 // main route
 router.get('/', (req, res) => {
-  console.log(process.env.RANDOM_ENV_VAR)
-  // res.send('Hello, backend!');
   res.render('index.ejs');
 });
 
@@ -20,108 +18,153 @@ router.get('/', (req, res) => {
 // When user searches for a movie
 router.get('/results', (req, res) => {
 
+  let context = {}
+  let url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}`
 
-    let context = {}
+  // let s = req.query.s
+  let s = req.query.s
+  let y = req.query.y
+  let plot = req.query.plot
 
-    // Make a request for the api
 
-    let url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}`
+  console.log(`The plot is: ${plot}`)
 
-    // let s = req.query.s
-    let s = req.query.s
-    let y = req.query.y
-    let plot = req.query.plot
-    
+  // concat the query
+  if (s !== '') {
+    url = url + `&s=${s}`
+  }
 
-    console.log(`The plot is: ${plot}`)
+  // if(y !== ''){
+  //     url = url + `&y=${y}`
+  // }
 
-    // concat the query
-    if(s !== ''){
-        url = url + `&s=${s}`
-    }
-
-    // if(y !== ''){
-    //     url = url + `&y=${y}`
-    // }
-
-    // if(plot === 'full'){
-    //     url = url + `&plot=${plot}`
-    // }
+  // if(plot === 'full'){
+  //     url = url + `&plot=${plot}`
+  // }
 
 
 
-    console.log(url)
+  console.log(url)
 
-    axios.get(url)
+  // Make a request for the api
+  axios.get(url)
     .then((response) => {
-    // handle success
-    console.log(response.data.Search);
+      // handle success
+      console.log(response.data.Search);
 
 
-    // Process the data
-    context.moviesList = response.data.Search
+      // Process the data
+      context.moviesList = response.data.Search
 
-    // Render the page incase of success
-    res.render('results.ejs', context);
+      // Render the page incase of success
+      res.render('results.ejs', context);
     })
     .catch((error) => {
-    // handle error
-    console.log(error);
+      // handle error
+      console.log(error);
+      res.send('Error!!')
     })
     .finally(() => {
-    // always executed
+      // always executed
     });
 
 
 
-  });
+});
 
 
 //   GET /movies/:movie_id
 
 router.get('/movies/:movie_id', (req, res) => {
 
-    let context = {}
-
-    // Make a request for the api
-
-    let url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}`
-
-    // imdb key
-    let i = req.params.movie_id
+  let context = {}
 
 
-    if(i){
-        url = url + `&i=${i}`
-    }
+  let url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}`
+
+  // imdb key
+  let i = req.params.movie_id
 
 
+  if (i) {
+    url = url + `&i=${i}`
+  }
 
-    console.log(url)
+  console.log(url)
 
-    axios.get(url)
+  // Make a request for the api
+  axios.get(url)
     .then((response) => {
-    // handle success
-    console.log(response.data);
+      // handle success
+      console.log(response.data);
 
 
-    // Process the data
-    // send to views
-    context.movie = response.data
+      // Process the data
+      // send to views
+      context.movie = response.data
 
 
-    // Render the page incase of success
-    res.render('detail.ejs', context);
+      // Render the page incase of success
+      res.render('detail.ejs', context);
     })
     .catch((error) => {
-    // handle error
-    console.log(error);
+      // handle error
+      res.send('Error!!')
     })
     .finally(() => {
-    // always executed
+      // always executed
     });
 
-    
+
+})
+
+// When user does the add to favorite form
+router.post('/faves', (req, res) => {
+
+  const newTitle = req.body.title
+  const newImdbid = req.body.imdbid
+
+  // Add the data to db
+  db.fave.create({
+    title: newTitle,
+    imdbid: newImdbid
+  })
+  .then(() => {
+
+    console.log("New Favorite Added")
+
+    res.redirect('/faves')
+  })
+  .catch(err => {
+    console.log(err)
+    res.send('Error!!')
+  })
+
+
+})
+
+
+// Load up the faves page
+router.get('/faves', (req, res) => {
+
+  let context = {}
+
+  // database function
+  db.fave.findAll()
+    .then(allFaves => {
+
+      console.log(allFaves)
+
+      context.allFaves = allFaves
+
+      res.render('faves.ejs', context)
+    })
+    .catch(err => {
+      console.log(err)
+
+      res.send('Error!!')
+    })
+
 })
 
 
