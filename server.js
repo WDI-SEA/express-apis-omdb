af82fbad2490cdea092be95fff44710022dacc8f
fjalafoo <fjalafoo@gmail.com>
2022-10-18 20:37:12 +0300

    p1

--- server.js
+++ server.js
@@ -19,15 +19,40 @@ app.use(require('morgan')('dev'));
 // Routes
 app.get('/', function(req, res) {
   console.log(process.env.RANDOM_ENV_VAR)
-  res.send('Hello, backend!');
+  // res.send('Hello, backend!');
+  res.render('index.ejs');
 });
 
 app.get('/results', function(req, res) {
   // req.query
+  console.log(req.query)
+  // let results
+  axios.get(`http://www.omdbapi.com/?s=${req.query.q}&apikey=${process.env.OMDB_API_KEY}`)
+  .then(results=>{
+    console.log(results.data)
+    res.render('results.ejs', {results: results.data.Search})
+  })
+  .catch(err=>{
+    console.log("oop! there was an issue retrieving API data!")
+  })
+
 });
 
+
+app.get('/movies/:id', (req, res)=>{
+  console.log(req.params)
+  axios.get(`http://www.omdbapi.com/?i=${req.params.id}&apikey=${process.env.OMDB_API_KEY}`)
+  .then(movieDetails=>{
+    console.log(movieDetails.data)
+    res.render('detail.ejs', movieDetails.data)
+  })
+  .catch(err=>console.log(err))
+})
+
+
+
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+const server = app.listen(process.env.PORT || 3000);
 
 // We can export this server to other servers like this
 module.exports = server;
