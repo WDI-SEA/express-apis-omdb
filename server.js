3c7cec1117f0149ce23eefea913157b7e6419c57
user <ranyamahmood55@gmail.com>
2022-10-19 08:38:39 +0300

    solved

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
+
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,6 +19,7 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
+  console.log(process.env.RANDOM_ENV_VAR)
   res.send('Hello, backend!');
 });
 
@@ -25,3 +28,19 @@ var server = app.listen(process.env.PORT || 3000);
 
 // We can export this server to other servers like this
 module.exports = server;
+
+
+// let results
+app.get('/results', (req,res)=>{
+  axios.get(`http://www.omdbapi.com/?&apikey=${process.env.API_KEY}&s=${req.query.s}`)
+  .then(function (response) {
+    res.render('results', {movies: response.data.Search})
+  })
+})
+
+app.get('/movies/:id', (req,res)=>{
+  axios.get(`http://www.omdbapi.com/?&apikey=${process.env.API_KEY}&i=${req.params.id }`)
+  .then(function(response){
+    res.render('detail', {movie: response.data})
+  })
+})
