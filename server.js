ec2873ba5bfa73eb2dc13ce91fcf444a9bbc320d
Loooai10 <honey_moonn@yahoo.com>
2022-10-19 07:44:33 +0300

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios');
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -18,9 +19,31 @@ app.use(require('morgan')('dev'));
 // Routes
 app.get('/', function(req, res) {
   console.log(process.env.RANDOM_ENV_VAR)
-  res.send('Hello, backend!');
+  // res.send('Hello, backend!');
+  res.render('index.ejs')
 });
 
+app.get('/results', (req,res)=>{
+  console.log(req.query)
+  //let results
+  axios.get(`http://www.omdbapi.com/?s=${req.query.q}&apikey=${process.env.OMDB_API_KEY}`)
+  .then(results=>{
+    console.log(results.data)
+    // because i want the data to show 
+    res.render('results.ejs', {results: results.data.Search})
+  })
+  .catch(err=>{
+    console.log('oop! there was an issue restrieving API', err)
+  })
+})
+app.get('/movies/:id', (req, res)=>{
+  console.log(req.params)
+  axios.get(`http://www.omdbapi.com/?i=${req.params.id}&apikey=${process.env.OMDB_API_KEY}`)
+  .then(movieDetails=>{
+    console.log(movieDetails.data)
+    res.render('detail.ejs', movieDetails.data)
+  })
+})
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
 
